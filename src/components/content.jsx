ca54d98e16aadb9d6a2d3b65cf78201045492124
Henry Quillin <henryquillin@gmail.com>
2022-01-03 18:35:21 -0600

    Added tabs and routing

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,33 +1,88 @@
 import React from "react";
 
-import { Box, Tabs, Tab } from "@mui/material";
-import { ArtistProfile } from "./artist-profile";
+import { Box, styled, Tabs, Tab } from "@mui/material";
+import { Songs } from "./songs.jsx";
+import { borderColor } from "@mui/system";
 
+const StyledTab = styled(Tab)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  fontFamily: theme.typography.tabs.fontFamily,
+  fontWeight: theme.typography.tabs.fontWeight,
+  border: "none",
+  backgroundColor: "transparent",
+  textTransform: "capitalize",
+}));
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return <div {...other}>{value === index && <Box p={3}>{children}</Box>}</div>;
+}
 
-export const Content = () => {
-  const [value, setValue] = React.useState("one");
+export const Content = (props) => {
+  const { match, history } = props;
+  const { params } = match;
+  const { page } = params;
 
+  const tabNameToIndex = {
+    0: "songs",
+    1: "playlists",
+    2: "contributors",
+    3: "wallet",
+    4: "metrics",
+  };
+
+  const indexToTabName = {
+    songs: 0,
+    playlists: 1,
+    contributors: 2,
+    wallet: 3,
+    metrics: 4,
+
+  };
+
+  const [value, setValue] = React.useState(indexToTabName[page]);
   const handleChange = (event, newValue) => {
+    history.push(`/home/${tabNameToIndex[newValue]}`);
     setValue(newValue);
+
   };
 
   return (
     <Box
       sx={{ justifyContent: "center", marginLeft: "auto", marginRight: "auto" }}
     >
-      <Tabs centered
-        sx={{ justifyContent: "center" }}
+      <Tabs
+        centered
         value={value}
         onChange={handleChange}
-        textColor="primary"
+        textColor="secondary"
         indicatorColor="secondary"
         aria-label="secondary tabs example"
       >
-        <Tab value="one" label="Item One" />
-        <Tab value="two" label="Item Two" />
-        <Tab value="three" label="Item Three" />
+        <StyledTab label="Songs" />
+        <StyledTab label="Playlists" />
+        <StyledTab label="Contributors" />
+        <StyledTab label="Wallet" />
+        <StyledTab label="Metrics" />
       </Tabs>
+
+      <Box justifyContent="center">
+        <TabPanel value={value} index={0}>
+          <Songs />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Playlists
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Contributors
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Wallet
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Metrics
+        </TabPanel>
+      </Box>
     </Box>
   );
 };
---
+++ src/components/songs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Grid, Typography } from "@mui/material";
+import { ArtistProfile } from "./artist-profile";
+
+
+
+export const Songs = () => {
+  return (
+    <div>
+     <h1>Songs</h1>
+    </div>
+  );
+};
