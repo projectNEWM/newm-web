ba25ca09346e6f402aa3c045c3b99a667727e2e9
Henry Quillin <henryquillin@gmail.com>
2022-01-05 08:56:07 -0600

    added responsive scrolling to song grid

---
+++ src/components/song.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+import { Grid, Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { ArtistProfile } from "./artist-profile";
+import songData from "../mockData";
+
+const Song = (props) => {
+  const { id, songId, name, album_image, history } = props;
+
+  return (
+    <>
+      <Card
+        sx={{
+          height: "250px",
+          width: "250px",
+          color: "black",
+          background: "#0A0A0A 0% 0% no-repeat padding-box;",
+          textAlign: "center",
+          padding:"0px",
+          margin:"0px",
+
+          opacity: ".7",
+        }}
+        onClick={() => history.push(`/home/song/${id}`)}
+      >
+        <CardMedia
+          image={album_image}
+          style={{
+            width: "200px",
+            height: "200px",
+            borderRadius: "100px",
+            margin: "auto",
+            marginTop: "25px",
+          }}
+        />
+        <CardContent>{name}</CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default Song;
