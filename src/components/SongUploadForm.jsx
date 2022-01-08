7b4481af730d2ce63cb400156b73ca58e2accd10
Henry Quillin <henryquillin@gmail.com>
2022-01-07 18:21:54 -0600

    added styled text field

--- src/components/SongUploadForm.jsx
+++ src/components/SongUploadForm.jsx
@@ -2,6 +2,10 @@ import { Grid, Typography, Paper, Box } from "@mui/material";
 import React from "react";
 import { styled } from "@mui/material/styles";
 import TextInput from "./TextInput";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { TextField, Button } from "@mui/material";
+
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
@@ -10,21 +14,15 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: "grey",
 }));
 
-function FormRow() {
-  return (
-    <React.Fragment>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-    </React.Fragment>
-  );
-}
+const StyledTextField = styled(TextField)({
+  width: "325px",
+  minWidth:"100px",
+  height: "38px",
+  marginTop: "25px",
+  border: "1px solid #1D1D1D !important",
+  borderRadius: 4,
+  backgroundColor: "#121212",
+});
 
 export default function SongUploadForm(props) {
   return (
@@ -34,7 +32,14 @@ export default function SongUploadForm(props) {
           <Typography variant="formHeader">Add Your Next Big Hit</Typography>
           <Grid direction="column" container spacing={1}>
             <Grid item>
-              <TextInput />
+              <StyledTextField
+                color="primary"
+                size="small"
+                id="outlined-basic"
+                align="left"
+                label="Title"
+                variant="outlined"
+              />
             </Grid>
           </Grid>
         </Grid>
