27480fbbe7d1ee47c0028ac1b6ddafd010486586
Jose Mateo <jmateo6@outlook.com>
2022-01-08 23:30:09 -0800

    Optimize  useWindowDimensionsHook

--- src/hooks/useWindowDimensions.js
+++ src/hooks/useWindowDimensions.js
@@ -1,31 +1,31 @@
 import { useEffect, useState } from "react";
 
 export default function useWindowDimensions() {
-  const hasWindow = typeof window !== "undefined";
-  // TODO update these so that the dependencies for the hook dont
-  // change on every render
-  function getWindowDimensions() {
-    const width = hasWindow ? window.innerWidth : null;
-    const height = hasWindow ? window.innerHeight : null;
-    return {
-      height,
-      width,
-    };
-  }
-
-  function handleResize() {
-    setWindowDimensions(getWindowDimensions());
-  }
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState({});
 
   useEffect(
     () => {
+      const hasWindow = typeof window !== "undefined";
+
+      function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+          height,
+          width,
+        };
+      }
+
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+
       if (hasWindow) {
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
       }
     },
-    [getWindowDimensions, handleResize, hasWindow]
+    [windowDimensions]
   );
 
   return windowDimensions;
