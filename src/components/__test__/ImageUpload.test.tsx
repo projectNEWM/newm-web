bf7300f3b22910b029bc83a67633de641876991d
Jose Mateo <jmateo6@gmail.com>
2022-02-04 20:33:48 -0800

    Fix image upload test

--- src/components/__test__/ImageUpload.test.tsx
+++ src/components/__test__/ImageUpload.test.tsx
@@ -1,45 +1,83 @@
 /* eslint-disable testing-library/no-render-in-setup */
 import { fireEvent, render, screen, waitForElementToBeRemoved } from "@testing-library/react";
-
 import "@testing-library/jest-dom";
 
 import { ImageUpload } from "../ImageUpload";
 
 test("shows correct label", () => {
-  render(<ImageUpload dropzoneLabel="Test Dropzone Text" buttonLabel="Test Button Label" />);
+  render(createComponent());
+
   expect(screen.getByText("Test Dropzone Text")).toBeDefined();
-  expect(screen.getByText("Test Button Label")).toBeDefined();
+  expect(screen.getByText("Upload Image")).toBeDefined();
 });
 
 describe("When image is dropped", () => {
   beforeEach(() => {
-    render(<ImageUpload dropzoneLabel="Test Dropzone Text" buttonLabel="Upload Image" />);
+    // Mocking the upload to cloudinary
+    jest
+      .spyOn(global, "fetch")
+      .mockImplementation(jest.fn(() => Promise.resolve({ json: () => ({ public_id: "test" }) })) as jest.Mock);
+  });
 
-    // window.URL.createObjectURL = jest.fn().mockImplementation(() => "url");
-    const inputEl = screen.getByTestId("dropzone");
-    const file = new File(["test"], "test", { type: "image/jpg" });
-    Object.defineProperty(inputEl, "files", {
-      value: [file],
-    });
-    fireEvent.drop(inputEl);
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
   it("Loading icon is shown", async () => {
-    await waitForElementToBeRemoved(() => screen.queryByText("Test Dropzone Text"));
+    // arrange
+    render(createComponent());
+    const inputElement = createFileInputElement();
 
+    // act
+    fireEvent.drop(inputElement);
+
+    // assert
+    await waitForElementToBeRemoved(() => screen.queryByText("Upload Image"));
     expect(screen.queryByText("Test Dropzone Text")).toBeNull();
     expect(screen.queryByText("Upload Image")).toBeNull();
     expect(screen.getByTestId("loadingIcon")).toBeVisible();
   });
 
-  // this test is failing because the fake image cannot be rendered
-  // it("Image is shown", async () => {
-  //   await waitForElementToBeRemoved(() => screen.queryByText("Test Dropzone Text"));
+  it("uploads image", async () => {
+    // arrange
+    render(createComponent());
+    const inputElement = createFileInputElement();
 
-  //   expect(screen.getByTestId("loadingIcon")).toBeVisible();
+    // act
+    fireEvent.drop(inputElement);
+    await screen.findByTestId("loadingIcon");
+    const imageElement = screen.getByTestId("uploadedImage");
+    fireEvent.load(imageElement); // mocking cloudinary onLoad()
 
-  //   await waitForElementToBeRemoved(() => screen.queryByTestId("loadingIcon")); // failing here
-
-  //   expect(screen.getByTestId("uploadedImage")).toBeVisible();
-  // });
+    // assert
+    expect(screen.queryByTestId("loadingIcon")).toBeNull();
+    expect(screen.queryByText("Upload Image")).toBeNull();
+    expect(screen.queryByText("Test Dropzone Text")).toBeNull();
+    expect(screen.getByTestId("uploadedImage")).toBeVisible();
+  });
 });
+
+function createComponent() {
+  return <ImageUpload dropzoneLabel="Test Dropzone Text" buttonLabel="Upload Image" />;
+}
+
+function createFileInputElement() {
+  const inputEl = screen.getByTestId("drop-input");
+
+  const file = createFile("file.png", 1234, "image/png");
+
+  Object.defineProperty(inputEl, "files", {
+    value: [file],
+  });
+  return inputEl;
+}
+
+function createFile(name: string, size: number, type: string) {
+  const file = new File([], name, { type });
+  Object.defineProperty(file, "size", {
+    get() {
+      return size;
+    },
+  });
+  return file;
+}
