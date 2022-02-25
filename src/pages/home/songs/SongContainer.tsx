80547f17a36b72a3311dfb885c70091907e168b7
hhs1234567890 <matsan2005@icloud.com>
2022-02-24 20:32:58 -0600

    init

---
+++ src/pages/home/songs/SongContainer.tsx
@@ -0,0 +1,36 @@
+import { Box } from "@mui/material";
+import Popup from "components/Popup";
+import { History } from "history";
+import { useState } from "react";
+import SongGrid from "./SongGrid";
+import SongUploadForm from "./SongUploadForm";
+export interface SongsProps {
+  history: History;
+}
+
+const Songs = ({ history }: SongsProps) => {
+  const [openPopup, setOpenPopup] = useState(false);
+
+
+
+  return (
+    <>
+      <Popup
+        height="522px"
+        openPopup={ openPopup }
+        setOpenPopup={ setOpenPopup }
+      >
+        <SongUploadForm setOpenPopup={ setOpenPopup } />
+      </Popup>
+
+      <Box>
+        <SongGrid 
+          history= { history } 
+          setOpenPopup={ setOpenPopup }
+        />
+      </Box>
+    </>
+  );
+};
+
+export default Songs;
--- src/pages/home/songs/SongGrid.tsx
+++ src/pages/home/songs/SongGrid.tsx
@@ -1,34 +1,51 @@
-import { Box, Grid } from "@mui/material";
+import { Grid, Box, Typography} from "@mui/material";
+import { FilledButton } from "components";
 import addSong from "assets/images/add-song.png";
 import { useAppSelector } from "common";
-import Popup from "components/Popup";
 import { History } from "history";
 import { selectSongs } from "modules/song";
-import { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import AddSongCard from "./AddSongCard";
 import Song from "./Song";
-import SongUploadForm from "./SongUploadForm";
 
 export interface SongsProps {
   history: History;
+  setOpenPopup: Dispatch<SetStateAction<boolean>>;
 }
 
-const Songs = ({ history }: SongsProps) => {
-  const [openPopup, setOpenPopup] = useState(false);
+const Songs = ({ history, setOpenPopup }: SongsProps) => {
 
-  const songs = useAppSelector(selectSongs);
-
+//   const songs = useAppSelector(selectSongs);
+  const songs:any = [];
+  if(songs[0]=== undefined){
+      return(
+        <div style={ { height: "100%", width: "100%" } }>
+          <StyledTypography align="center" variant="h5">
+            Lets Get This Party Started
+          </StyledTypography>
+          <hr style={ { border: 0, borderTop: "1px solid #CC33CC" } } />
+          <div>
+            { /* IMAGE UPLOAD COMPONENT NOT WORKING */ }
+            <Box
+              sx={ {
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              } }
+            >
+            { /* DRAG AND DROP */ }
+              <Typography variant="body1" align="center" color="primary" marginTop="30px" marginBottom="27px">
+                OR
+              </Typography>
+              <FilledButton sx={ { width: "164px" } }>Upload Image</FilledButton>
+            </Box>
+        </div>
+      </div>
+      );
+  }
   return (
     <>
-      <Popup
-        height="522px"
-        openPopup={ openPopup }
-        setOpenPopup={ setOpenPopup }
-      >
-        <SongUploadForm setOpenPopup={ setOpenPopup } />
-      </Popup>
-
-      <Box>
         <Grid container>
           { Object.keys(songs).map(Number).map((songId) => {
             const { name, albumImage } = songs[songId];
@@ -68,7 +85,6 @@ const Songs = ({ history }: SongsProps) => {
             />
           </Grid>
         </Grid>
-      </Box>
     </>
   );
 };
