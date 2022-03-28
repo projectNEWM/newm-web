ea3a79562cbe03c614e1ef830b5b9c8a1a4d987a
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-03-27 23:34:31 -0700

    corrects typo

--- src/modules/session/slice.ts
+++ src/modules/session/slice.ts
@@ -1,23 +1,19 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import Cookie from "js-cookie";
 import { extendedApi } from "./api";
 
-interface RoleState {
+interface SessionState {
   isLoggedIn: boolean;
 }
 
-const initialState: RoleState = {
+const initialState: SessionState = {
   isLoggedIn: false,
 };
 
 const sessionSlice = createSlice({
   initialState,
   name: "session",
-  reducers: {
-    setIsLoggedIn(state, action: PayloadAction<boolean>) {
-      state.isLoggedIn = action.payload;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder.addMatcher(
       extendedApi.endpoints.googleLogin.matchFulfilled,
