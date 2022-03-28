ca00b67f380874671d04032b35304330e7147b53
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-03-27 19:36:12 -0700

    improves api set headers

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "store";
+import Cookies from "js-cookie";
 
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     // baseUrl: "https://staging-newm-server.herokuapp.com/",
     baseUrl: "https://newm-app-pip-wlara-cors-z40ymg.herokuapp.com/",
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+      const { isLoggedIn } = state.session;
+
+      if (isLoggedIn) {
+        headers.set("Authorization", `Bearer ${Cookies.get("apiToken")}`);
+      }
+
+      return headers;
+    },
   }),
   endpoints: () => ({}),
 });
--- src/modules/song/api.ts
+++ src/modules/song/api.ts
@@ -1,13 +1,9 @@
 import api from "api";
-import Cookies from "js-cookie";
 
 export const extendedApi = api.injectEndpoints({
   endpoints: (build) => ({
     getSongs: build.query({
-      query: () => ({
-        url: "v1/portal/songs",
-        headers: { Authorization: `Bearer ${Cookies.get("apiToken")}` },
-      }),
+      query: () => "v1/portal/songs",
     }),
   }),
 });
