b06ec6c42ef8780ff6760ff4f3a0085049d8dacc
Henry Quillin <henryquillin@gmail.com>
2022-03-30 16:56:44 -0500

    Added colors and gradients

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,6 +2,29 @@ import { createTheme } from "@mui/material/styles";
 
 declare module "@mui/material/styles" {
   interface Theme {
+    colors: {
+      blue: string;
+      purple: string;
+      red: string;
+      orange: string;
+      yellow: string;
+      green: string;
+      white: string;
+      grey100: string;
+      grey200: string;
+      grey300: string;
+      grey400: string;
+      grey500: string;
+      black: string;
+    };
+    gradients: {
+      company: string;
+      artist: string;
+      magazine: string;
+      product: string;
+      partners: string;
+      crypto: string;
+    };
     inputField: {
       background: string;
       border: string;
@@ -9,6 +32,29 @@ declare module "@mui/material/styles" {
   }
   // allow configuration using `createTheme`
   interface ThemeOptions {
+    colors?: {
+      blue?: string;
+      purple?: string;
+      red?: string;
+      orange?: string;
+      yellow?: string;
+      green?: string;
+      white?: string;
+      grey100?: string;
+      grey200?: string;
+      grey300?: string;
+      grey400?: string;
+      grey500?: string;
+      black?: string;
+    };
+    gradients?: {
+      company?: string;
+      artist?: string;
+      magazine?: string;
+      product?: string;
+      partners?: string;
+      crypto?: string;
+    };
     inputField?: {
       background?: string;
       border?: string;
@@ -38,6 +84,29 @@ declare module "@mui/material/Typography" {
 }
 
 export default createTheme({
+  colors: {
+    blue: "#0099CC",
+    purple: "#CC33CC",
+    red: "#FF3366",
+    orange: "#FF9900",
+    yellow: "#FFFF66",
+    green: "#66FF66",
+    white: "#FFFFFF",
+    grey100: "#8E8E93",
+    grey200: "#636366",
+    grey300: "#48484A",
+    grey400: "#2C2C2E",
+    grey500: "#1C1C1E",
+    black: "#000000",
+  },
+  gradients: {
+    company: "linear-gradient(53.48deg, #0099CC 0%, #CC33CC 100%);",
+    artist: "linear-gradient(53.48deg, #CC33CC 0%, #FF3366 100%);",
+    magazine: "linear-gradient(53.48deg, #FF3366 0%, #FFFF66 100%);",
+    product: "linear-gradient(53.48deg, #FF9900 0%, #FF3366 100%);",
+    partners: "linear-gradient(53.48deg, #FFFF66 0%, #FF9900 100%);",
+    crypto: "linear-gradient(53.48deg, #66FF66 0%, #0099CC 100%);",
+  },
   breakpoints: {
     values: {
       xs: 0,
@@ -47,7 +116,6 @@ export default createTheme({
       xl: 1536,
     },
   },
-
   components: {
     MuiUseMediaQuery: {
       defaultProps: {
@@ -74,6 +142,7 @@ export default createTheme({
     secondary: {
       main: "#CC33CC",
     },
+
     text: {
       primary: "#FFFFFF",
       secondary: "#707070",
