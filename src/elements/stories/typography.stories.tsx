869675880f79e516f3701c12ce14a0e1f1b7171f
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-04-03 13:13:07 -0700

    adds sm variant to Typography stories

--- src/elements/stories/typography.stories.tsx
+++ src/elements/stories/typography.stories.tsx
@@ -10,6 +10,7 @@ export const Variants = () => (
   <Stack mt={ 2 } direction="column" spacing={ 2 }>
     <Typography variant="xxs">Text xxs</Typography>
     <Typography variant="xs">Text xs</Typography>
+    <Typography variant="sm">Text sm</Typography>
     <Typography variant="md">Text md</Typography>
     <Typography variant="xxl">Text xxl</Typography>
   </Stack>
