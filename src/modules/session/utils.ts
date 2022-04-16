df126c6cccca9d59b6960013134ab8b7775f18ee
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-04-15 19:39:20 -0700

    adds expiration date to jwt token

--- src/modules/session/utils.ts
+++ src/modules/session/utils.ts
@@ -6,8 +6,8 @@ export const handleSuccessfulAuthentication = (
   state: SessionState,
   { payload }: PayloadAction<NewmAuthResponse>
 ) => {
-  Cookie.set("accessToken", payload.accessToken);
-  Cookie.set("refreshToken", payload.refreshToken);
+  Cookie.set("accessToken", payload.accessToken, { expires: 0.05 });
+  Cookie.set("refreshToken", payload.refreshToken, { expires: 10 });
 
   state.isLoggedIn = true;
   state.errorMessage = "";
