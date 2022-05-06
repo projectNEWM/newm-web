780257e305a3d591ee7248c6f2dca9975fadd626
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-05-05 20:45:05 -0700

    implements sidebar buttons

---
+++ src/elements/styled/TransparentButton.ts
@@ -0,0 +1,29 @@
+import { Button, ButtonProps } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import theme from "theme";
+
+interface TransparentButtonProps extends ButtonProps {
+  readonly active?: boolean;
+}
+
+const activeBackground = "rgba(255, 255, 255, 0.1)";
+
+const TransparentButton = styled(Button)<TransparentButtonProps>`
+  background: ${(props) => (props.active ? activeBackground : "transparent")};
+  border-radius: 7px;
+  font-size: ${theme.button.fontSize};
+  line-height: ${theme.button.lineHeight};
+  font-weight: ${theme.button.fontWeight};
+  color: white;
+  font: ${theme.typography.button.font};
+  text-transform: none;
+  padding: 12px 16px;
+  opacity: ${(props) => (props.disabled || !props.active ? 0.5 : 1)};
+  transition: background-color 0ms;
+  &:hover {
+    opacity: 1;
+    background: ${activeBackground};
+  }
+`;
+
+export default TransparentButton;
--- src/pages/home/SideBar.tsx
+++ src/pages/home/SideBar.tsx
@@ -1,11 +1,20 @@
 import { FunctionComponent } from "react";
 import { Box, Stack } from "@mui/material";
 import { Typography } from "elements";
-import { ProfileImage } from "components";
+import { ProfileImage, SideBarButton } from "components";
 import { useSelector } from "react-redux";
 import { selectSession } from "modules/session";
 import { useTheme } from "@mui/material/styles";
+import UploadIcon from "assets/images/UploadIcon";
 import NewmLogoSmInverse from "assets/images/NEWM-logo-sm-inverse";
+
+const buttons = [
+  {
+    icon: <UploadIcon />,
+    label: "UPLOAD SONG",
+    to: "/home/upload-song",
+  },
+];
 
 const SideBar: FunctionComponent = () => {
   const theme = useTheme();
@@ -21,19 +30,26 @@ const SideBar: FunctionComponent = () => {
         background: theme.colors.black,
         flexDirection: "column",
         justifyContent: "space-between",
-        alignItems: "center",
         padding: 1.25,
       } }
     >
-      <Stack mt={ 3.75 } spacing={ 2 }>
-        { !!profile.pictureUrl && (
-          <ProfileImage src={ profile.pictureUrl } aria-label="profile image" />
-        ) }
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Stack mt={ 3.75 } spacing={ 2 }>
+          { !!profile.pictureUrl && (
+            <ProfileImage src={ profile.pictureUrl } aria-label="profile image" />
+          ) }
 
-        <Typography variant="md" fontWeight="bold" align="center">
-          { profile.nickname }
-        </Typography>
-      </Stack>
+          <Typography variant="md" fontWeight="bold" align="center">
+            { profile.nickname }
+          </Typography>
+        </Stack>
+
+        <Stack mt={ 10 } sx={ { width: "100%" } }>
+          { buttons.map(({ icon, label, to }) => (
+            <SideBarButton key={ label } icon={ icon } label={ label } to={ to } />
+          )) }
+        </Stack>
+      </Box>
 
       <Box px={ 2.5 } pb={ 2.5 } width="100%" justifyContent="flex-start">
         <NewmLogoSmInverse />
