3621385f28cbd9905994da82eede6529f13bf139
Jonatan Escobar <escobarjonatan@aggienetwork.com>
2022-05-10 15:05:07 -0500

    Deleting signup utils file missed by merge

--- src/pages/signUp/utils.ts
+++
@@ -1,7 +0,0 @@
-import { FormikValues } from "formik";
-import { extendedApi as sessionApi } from "modules/session";
-
-const sendVerificationEmail = (values: FormikValues) =>
-  sessionApi.endpoints.sendVerificationEmail.initiate({ email: values.email });
-
-export { sendVerificationEmail };
