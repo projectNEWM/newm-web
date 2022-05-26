9c155fc52a6954114e4175c6707c280b5165c516
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-05-25 23:19:52 -0700

    reverts Gradient Typopgraphy component to use mui styled function

--- src/elements/styled/GradientTypography.ts
+++ src/elements/styled/GradientTypography.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { styled } from "@mui/material/styles";
 import theme from "theme";
 import Typography from "../Typography";
 
@@ -6,7 +6,6 @@ const GradientTypography = styled(Typography)`
   color: transparent;
   background-color: ${theme.colors.red};
   background: ${theme.gradients.artist};
-  -webkit-background-clip: text;
   background-clip: text;
   text-fill-color: transparent;
   text-shadow: 0 0 transparent;
