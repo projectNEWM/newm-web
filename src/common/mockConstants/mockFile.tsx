c380c7979b8434650a92fa3b714ff19f6951af19
henry quillin <henryquillin@gmail.com>
2022-06-17 10:56:44 -0500

    Make mock constants folder

---
+++ src/common/mockConstants/mockFile.tsx
@@ -0,0 +1,12 @@
+export const mockFile = {
+  name: "hello-world.txt",
+  size: 1000,
+  type: "",
+  webkitRelativePath: "/",
+  lastModified: 1234567890,
+  preview: "",
+  arrayBuffer: (() => {}) as any, // eslint-disable-line
+  slice: (() => {}) as any, // eslint-disable-line
+  stream: (() => {}) as any, // eslint-disable-line
+  text: (() => {}) as any, // eslint-disable-line
+};
