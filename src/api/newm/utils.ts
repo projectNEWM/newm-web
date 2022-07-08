061f5f26713dca781f9deeeff7ece17d9acf06b4
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-07-07 18:19:39 -0700

    uses async-mutex to ensure multiple refresh calls are not made

--- src/api/newm/utils.ts
+++ src/api/newm/utils.ts
@@ -4,11 +4,15 @@ import {
   FetchBaseQueryError,
 } from "@reduxjs/toolkit/query";
 import Cookies from "js-cookie";
+import { Mutex } from "async-mutex";
 import { logOut, receiveRefreshToken } from "modules/session";
+
+const mutex = new Mutex();
 
 /**
  * Wraps a base query with functionality to refresh the access token
- * if it is no longer valid and retry the request.
+ * if it is no longer valid and retry the request. Utilizes Mutex to prevent
+ * simultaneous calls to refresh the token.
  */
 export const fetchBaseQueryWithReauth = (
   baseQuery: BaseQueryFn<
@@ -16,44 +20,66 @@ export const fetchBaseQueryWithReauth = (
     any, // eslint-disable-line @typescript-eslint/no-explicit-any
     FetchBaseQueryError
   >
-) => {
-  const baseQueryWithReauth: BaseQueryFn<
-    string | FetchArgs,
-    unknown,
-    FetchBaseQueryError
-  > = async (args, api, extraOptions) => {
+): BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> => {
+  return async (args, api, extraOptions) => {
+    const makeRequestWithRefreshedAccessToken = async () => {
+      const accessToken = Cookies.get("accessToken");
+
+      const argsWithHeader =
+        typeof args === "string"
+          ? {
+              url: args,
+              headers: { Authorization: `Bearer ${accessToken}` },
+            }
+          : {
+              ...args,
+              headers: { Authorization: `Bearer ${accessToken}` },
+            };
+
+      return await baseQuery(argsWithHeader, api, extraOptions);
+    };
+
+    // wait until the mutex is available without locking it
+    await mutex.waitForUnlock();
+
+    // get current refresh token
     const refreshToken = Cookies.get("refreshToken");
 
+    // attempt request
     let result = await baseQuery(args, api, extraOptions);
 
-    if (result.error && result.error.status === 401 && refreshToken) {
-      const refreshResult = await baseQuery(
-        {
-          url: "/v1/auth/refresh",
-          headers: { Authorization: `Bearer ${refreshToken}` },
-        },
-        api,
-        extraOptions
-      );
+    if (result.error?.status === 401 && refreshToken) {
+      // check whether the mutex is unlocked before attempting refresh
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire();
 
-      if (refreshResult.data) {
-        api.dispatch(receiveRefreshToken(refreshResult.data));
+        try {
+          // get new access and refresh token
+          const refreshResult = await baseQuery(
+            {
+              url: "/v1/auth/refresh",
+              headers: { Authorization: `Bearer ${refreshToken}` },
+            },
+            api,
+            extraOptions
+          );
 
-        // re-attempt request with refreshed access token
-        const accessToken = Cookies.get("accessToken");
-        const argsWithHeader =
-          typeof args === "string"
-            ? { url: args, headers: { Authorization: `Bearer ${accessToken}` } }
-            : { ...args, headers: { Authorization: `Bearer ${accessToken}` } };
+          if (refreshResult.data) {
+            api.dispatch(receiveRefreshToken(refreshResult.data));
 
-        result = await baseQuery(argsWithHeader, api, extraOptions);
+            result = await makeRequestWithRefreshedAccessToken();
+          } else {
+            api.dispatch(logOut());
+          }
+        } finally {
+          release();
+        }
       } else {
-        api.dispatch(logOut());
+        await mutex.waitForUnlock();
+        result = await makeRequestWithRefreshedAccessToken();
       }
     }
 
     return result;
   };
-
-  return baseQueryWithReauth;
 };
