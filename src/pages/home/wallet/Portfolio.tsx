3982440d02c34df38fcbad42d366717cc64e21c0
Henry Quillin <henryquillin@gmail.com>
2022-08-14 19:16:12 -0500

    Add skeleton to Portfolio

--- src/pages/home/wallet/Portfolio.tsx
+++ src/pages/home/wallet/Portfolio.tsx
@@ -1,14 +1,28 @@
-import { FunctionComponent, useState } from "react";
-import { SongRoyalties, mockSongRoyalties } from "common";
-import { Box, CircularProgress  } from "@mui/material";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+import { SongRoyalties, mockSongRoyalties, useWindowDimensions } from "common";
+import { Box } from "@mui/material";
+import SkeletonTable from "elements/skeletons/TableSkeleton";
+import theme from "theme";
 import AllCaughtUp from "./AllCaughtUp";
 import SongRoyaltiesList from "./SongRoyaltiesList";
 
 const Portfolio: FunctionComponent = () => {
   const { data = [], isLoading, isSuccess } = mockSongRoyalties;
   const songRoyalties: SongRoyalties[] = data;
-
+  const windowHeight = useWindowDimensions()?.height;
+  const windowWidth = useWindowDimensions()?.width;
+  const maxListWidth = 700;
+  const skeletonRef = useRef<any>();
+  const skeletonYPos = skeletonRef && skeletonRef.current?.offsetTop;
+  const [skeletonRows, setSkeletonRows] = useState<number>(10);
   const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setSkeletonRows(
+      windowHeight ? Math.floor((windowHeight - skeletonYPos - 200) / 50) : 10
+    );
+  }, [windowHeight, skeletonYPos]);
 
   const renderContent = (
     isLoading: boolean,
@@ -18,14 +32,14 @@ const Portfolio: FunctionComponent = () => {
     if (isLoading) {
       return (
         <>
-          <Box
-            sx={ {
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            } }
-          >
-            <CircularProgress color="secondary" />
+          <Box ref={ skeletonRef }>
+            <SkeletonTable
+              cols={
+                windowWidth && windowWidth > theme.breakpoints.values.sm ? 3 : 2
+              }
+              rows={ skeletonRows }
+              maxWidth={ maxListWidth }
+            />
           </Box>
         </>
       );
@@ -48,7 +62,9 @@ const Portfolio: FunctionComponent = () => {
     }
   };
   return (
-    <Box paddingTop={ 2 }>{ renderContent(isLoading, isSuccess, songRoyalties) }</Box>
+    <Box paddingTop={ 2 }>
+      { renderContent(isLoading, isSuccess, songRoyalties) }
+    </Box>
   );
 };
 
