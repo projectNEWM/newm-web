181eaaec56607d8a9492403231079395736ef01e
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2022-09-20 15:09:24 -0700

    minor comment update in build params

--- src/buildParams.ts
+++ src/buildParams.ts
@@ -4,7 +4,7 @@ export const isMainNet = process.env.REACT_APP_NETWORK_MODE === "1";
 // Change this value to disable Redux logging in development
 export const enableReduxLogging = true;
 
-// TODO: Update with production / MainNet url hosts
+// TODO: Update with NEWM server production url host
 export const baseUrls: Record<string, string> = {
   newm: isProd
     ? "https://staging-newm-server.herokuapp.com/"
