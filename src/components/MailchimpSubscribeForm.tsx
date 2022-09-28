48c69f4268b05d324ed78e69e1e8a9f92c168fa9
escobarjonatan <escobarjonatan@aggienetwork.com>
2022-09-27 23:37:00 -0500

    Adding mailchimp to artist signup

---
+++ src/components/MailchimpSubscribeForm.tsx
@@ -0,0 +1,89 @@
+import { Stack, useMediaQuery } from "@mui/material";
+import { FilledButton, TextInput } from "elements";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+import theme from "theme";
+
+interface MailchimpSubscribeFormProps {
+  readonly u: string;
+  readonly id: string;
+  readonly fId: string;
+  readonly hiddenInputName: string;
+  readonly groupName?: string;
+}
+
+/**
+ * Email subscription form based on generated Mailchimp template
+ */
+const MailchimpSubscribeForm: FunctionComponent<
+  MailchimpSubscribeFormProps
+> = ({ u, id, fId, hiddenInputName, groupName }) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("md"));
+
+  useEffect(() => {
+    const currentInputRef = inputRef.current;
+    const preventDefault = (event: Event) => event.preventDefault();
+
+    currentInputRef?.addEventListener("invalid", preventDefault);
+
+    return () =>
+      currentInputRef?.removeEventListener("invalid", preventDefault);
+  });
+
+  const handleBlur = () => {
+    setIsValid(!!inputRef.current?.validity.valid);
+  };
+
+  return (
+    <form
+      action={ `https://projectnewm.us1.list-manage.com/subscribe/post?u=${u}&amp;id=${id}&amp;f_id=${fId}` }
+      method="post"
+      target="_self"
+      style={ { marginTop: theme.spacing(0.5) } }
+    >
+      { !!groupName && (
+        <input
+          style={ { display: "none" } }
+          type="checkbox"
+          value="4"
+          name={ groupName }
+          checked={ true }
+        />
+      ) }
+
+      <div style={ { position: "absolute", left: "-5000px" } } aria-hidden="true">
+        <input type="text" name={ hiddenInputName } tabIndex={ -1 } value="" />
+      </div>
+
+      <Stack direction={ isLargeScreen ? "row" : "column" } spacing={ 1.5 }>
+        <TextInput
+          id={ id }
+          type="email"
+          name="EMAIL"
+          value={ email }
+          placeholder="Email address"
+          onChange={ (e) => setEmail(e.target.value) }
+          widthType={ isLargeScreen ? "default" : "full" }
+          required={ true }
+          onBlur={ handleBlur }
+          ref={ inputRef }
+          errorMessage={ isValid ? "" : "Please enter a valid email address" }
+        />
+        <FilledButton
+          disabled={ !isValid }
+          name="subscribe"
+          sx={ { maxHeight: "46px" } }
+          type="submit"
+          value="Subscribe"
+        >
+          Subscribe
+        </FilledButton>
+      </Stack>
+    </form>
+  );
+};
+
+export default MailchimpSubscribeForm;
--- src/pages/sampleSale/Congratulations.tsx
+++ src/pages/sampleSale/Congratulations.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import { GradientTypography, Typography } from "elements";
 import { ResponsiveNEWMLogo } from "components";
+import MailchimpSubscribeForm from "components/MailchimpSubscribeForm";
 import { Box, Stack, useTheme } from "@mui/material";
 import DiscordIcon from "assets/images/DiscordIcon";
 import TelegramIcon from "assets/images/TelegramIcon";
@@ -16,6 +17,7 @@ const Congratulations: FunctionComponent = () => {
         display: "flex",
         flex: 1,
         flexDirection: "column",
+        height: "100%",
         justifyContent: "space-between",
         maxWidth: "100%",
         pt: 7.5,
@@ -37,19 +39,37 @@ const Congratulations: FunctionComponent = () => {
         >
           One giant leap for music!
         </GradientTypography>
-        <Stack maxWidth="580px" mt={ 7.5 } px={ 2.5 } spacing={ 1.5 }>
+        <Stack maxWidth="580px" mt={ 5 } px={ 2.5 } spacing={ 1.5 }>
           <Typography variant="h3">What now?</Typography>
           <Typography fontWeight={ 400 }>
             Time to kick back, relax and keep an eye on your inbox. When the
-            Artist Portal is ready, we’ll send you an email with your all-access
-            pass for pre-launch. From there, you can finish setting up your
-            artist profile, upload & mint music and get ready to watch the
-            royalties roll in.
+            Artist Portal is ready, we&apos;ll send you an email with your
+            all-access pass for pre-launch. From there, you can finish setting
+            up your artist profile, upload & mint music and get ready to watch
+            the royalties roll in.
           </Typography>
+          <label
+            style={ {
+              color: theme.colors.grey100,
+              fontSize: "12px",
+              textAlign: "left",
+              marginTop: theme.spacing(5),
+            } }
+            htmlFor="460ff42d47"
+          >
+            GET NOTIFIED ABOUT LAUNCH
+          </label>
+          <MailchimpSubscribeForm
+            fId="000e7ae2f0"
+            groupName="group[380982][4]"
+            hiddenInputName="b_3bf911620d8791d21fb973749_460ff42d47"
+            id="460ff42d47"
+            u="3bf911620d8791d21fb973749"
+          />
         </Stack>
       </Box>
 
-      <Stack mb={ 4 } mt={ 2 } direction="row" columnGap={ 4 }>
+      <Stack pb={ 4 } mt={ 4 } direction="row" columnGap={ 4 }>
         <a
           aria-label="View our Telegram channel"
           href="https://t.me/NEWMofficial"
