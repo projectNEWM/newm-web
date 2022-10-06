2b76c52e44e4273469c4d608b78e21ffb6e5ebc9
escobarjonatan <escobarjonatan@aggienetwork.com>
2022-10-06 14:53:46 -0500

    Updating production URL

--- src/buildParams.ts
+++ src/buildParams.ts
@@ -3,10 +3,9 @@ export const isProd = process.env.NODE_ENV === "production";
 // Change this value to disable Redux logging in development
 export const enableReduxLogging = !isProd && true;
 
-// TODO: Update with NEWM server production url host
 export const baseUrls: Record<string, string> = {
   newm: isProd
-    ? "https://staging-newm-server.herokuapp.com/"
+    ? "https://studio.newm.io/"
     : "https://staging-newm-server.herokuapp.com/",
   phyrhose: isProd
     ? "https://api.phyrhose.io/"
