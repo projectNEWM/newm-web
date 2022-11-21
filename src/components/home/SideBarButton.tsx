5f7669ac815b1b51be28a2d4ffec1d2fe454d4b9
dmkirshon <kirshon.david@gmail.com>
2022-11-21 14:25:23 +0200

    feat: Transition the SideBarButton into a nav link

--- src/components/home/SideBarButton.tsx
+++ src/components/home/SideBarButton.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from "react";
-import { TransparentButton } from "elements";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { Stack } from "@mui/material";
+import theme from "theme";
 
 interface SideBarButtonProps {
   readonly label: string;
@@ -10,34 +10,54 @@ interface SideBarButtonProps {
   readonly closeMenu?: VoidFunction;
 }
 
+const activeBackground = "rgba(255, 255, 255, 0.1)";
+
 const SideBarButton: FunctionComponent<SideBarButtonProps> = ({
   label,
   icon,
   to,
   closeMenu,
-  ...rest
 }) => {
   const resolved = useResolvedPath(to);
   const match = useMatch(resolved.pathname);
 
   return (
-    <Link to={ to } style={ { textDecoration: "none" } }>
-      <TransparentButton
-        key={ label }
-        active={ !!match }
-        onClick={ closeMenu }
-        fullWidth={ true }
+    <Link
+      key={ label }
+      onClick={ closeMenu }
+      to={ to }
+      style={ {
+        textDecoration: "none",
+        minWidth: "100%",
+      } }
+    >
+      <Stack
+        direction="row"
+        spacing={ 2.5 }
         sx={ {
+          fontSize: theme.button.fontSize,
+          lineHeight: theme.button.lineHeight,
+          fontWeight: theme.button.fontWeight,
+          font: theme.typography.button.font,
           alignItems: "center",
+          borderRadius: "7px",
+          padding: "12px 20px",
+          color: "white",
+          background: match ? activeBackground : "transparent",
+          opacity: `${!match ? 0.5 : 1}`,
+          transition: "background-color 0ms",
+          display: "flex",
+          textTransform: "none",
           justifyContent: "flex-start",
+          "&:hover": {
+            opacity: "1",
+            background: `${activeBackground}`,
+          },
         } }
-        { ...rest }
       >
-        <Stack direction="row" spacing={ 2.5 }>
-          { icon }
-          <span>{ label }</span>
-        </Stack>
-      </TransparentButton>
+        { icon }
+        <span>{ label }</span>
+      </Stack>
     </Link>
   );
 };
