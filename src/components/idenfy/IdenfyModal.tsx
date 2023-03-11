e87e158c42d1b393537778873e5f61b203aec371
escobarjonatan <escobarjonatan11@gmail.com>
2023-03-11 16:04:05 -0600

    Extracting profile fetching to the app so that users are notified when they become verified

--- src/components/idenfy/IdenfyModal.tsx
+++ src/components/idenfy/IdenfyModal.tsx
@@ -1,15 +1,9 @@
 import { FunctionComponent, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { CircularProgress, Stack } from "@mui/material";
 import Cookies from "js-cookie";
 import { Modal } from "components";
-import {
-  getIdenfyAuthToken,
-  removeVerificationTimer,
-  selectSession,
-  extendedApi as sessionApi,
-  startVerificationTimer,
-} from "modules/session";
+import { getIdenfyAuthToken } from "modules/session";
 import theme from "theme";
 
 interface IdenfyModalProps {
@@ -23,37 +17,17 @@ const IdenfyModal: FunctionComponent<IdenfyModalProps> = ({
   onClose,
 }) => {
   const dispatch = useDispatch();
-  const [shouldRequestProfile, setShouldRequestProfile] = useState(false);
   const [idenfyAuthToken, setIdenfyAuthToken] = useState(
     Cookies.get("idenfyAuthToken")
   );
-  const { verificationPingStartedAt, profile: { verificationStatus } = {} } =
-    useSelector(selectSession);
-  const isVerified = verificationStatus === "Verified";
 
   if (!idenfyAuthToken) {
     dispatch(getIdenfyAuthToken());
   }
 
-  /** Removes verification timer when verified. */
-  if (isVerified && verificationPingStartedAt) {
-    dispatch(removeVerificationTimer());
-  }
-
-  /**
-   * Listens for success message event.
-   * Triggers the verification timer to start when event is received.
-   */
+  /** Listens for modal close message. */
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      if (event?.data === "idenfy-verification-success") {
-        setShouldRequestProfile(true);
-
-        if (!verificationPingStartedAt) {
-          dispatch(startVerificationTimer());
-        }
-      }
-
       if (event?.data === "idenfy-modal-close") {
         const event = new Event("close");
 
@@ -66,28 +40,7 @@ const IdenfyModal: FunctionComponent<IdenfyModalProps> = ({
     return () => {
       window.removeEventListener("message", handleMessage);
     };
-  }, [dispatch, onClose, verificationPingStartedAt]);
-
-  /**
-   * Requests profile information at 1 minute interval on success page.
-   * Clears interval when it has been more than 20 minutes or user is verified.
-   */
-  useEffect(() => {
-    if (shouldRequestProfile && verificationPingStartedAt) {
-      const TWENTY_MINUTES = 20 * 60 * 1000;
-      const pingExpiry = verificationPingStartedAt + TWENTY_MINUTES;
-      const hasExceededAllowedTime = verificationPingStartedAt > pingExpiry;
-      const verificationStatusInterval = setInterval(() => {
-        dispatch(sessionApi.endpoints.getProfile.initiate());
-      }, 60000);
-
-      if (hasExceededAllowedTime || isVerified) {
-        clearInterval(verificationStatusInterval);
-      }
-
-      return () => clearInterval(verificationStatusInterval);
-    }
-  }, [dispatch, isVerified, shouldRequestProfile, verificationPingStartedAt]);
+  }, [onClose]);
 
   /**
    * Gets "idenfyAuthToken" cookie at 1 second interval.
--- src/components/idenfy/IdenfySuccessSession.tsx
+++ src/components/idenfy/IdenfySuccessSession.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent, useEffect } from "react";
 import { Stack, Typography } from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Button, GradientTypography } from "elements";
 import theme from "theme";
 
@@ -22,24 +21,20 @@ const IdenfySuccessSession: FunctionComponent = () => {
         justifyContent: "center",
       } }
     >
-      <Typography variant="h1">CONGRATS!</Typography>
+      <Typography variant="h1">THANK YOU!</Typography>
       <GradientTypography
         style={ { ...theme.typography.emphasized } }
         variant="h1"
       >
-        You&apos;re now verified.
+        You will be notified soon.
       </GradientTypography>
       <Typography
         sx={ {
-          alignItems: "ceneter",
-          columnGap: 1.5,
-          display: "flex",
           fontWeight: 400,
           my: [2, 3, 4],
         } }
       >
-        <CheckCircleIcon color="success" />
-        You can now mint your songs and claim royalties.
+        Get ready to mint your songs and claim royalties.
       </Typography>
       <Button onClick={ handleClick }>Got it</Button>
     </Stack>
