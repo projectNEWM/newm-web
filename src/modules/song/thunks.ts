b12b722b146505faf41d5edd029d778aeb3283e0
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2023-04-06 01:07:37 -0700

    merges in master

--- src/modules/song/thunks.ts
+++ src/modules/song/thunks.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getFileBinary } from "common";
 import { history } from "common/history";
 import { uploadToCloudinary } from "api/cloudinary/utils";
 import {
@@ -57,13 +56,12 @@ export const uploadSong = createAsyncThunk(
       if ("error" in audioUploadUrlResp) return;
 
       const { uploadUrl } = audioUploadUrlResp.data;
-      const audioBinaryStr = await getFileBinary(body.audio);
 
       // upload audio to AWS, song audioUrl will be updated after it's transcoded
       await fetch(uploadUrl, {
         method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ file: audioBinaryStr }),
+        headers: { "Content-Type": "application/octet-stream" },
+        body: body.audio,
       });
 
       // navigate to library page to view new song
