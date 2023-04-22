730cb8780a459065a3d0e656e118d2e4ff59ccea
escobarjonatan <escobarjonatan11@gmail.com>
2023-04-21 22:59:31 -0500

    Removing accidental change

--- src/buildParams.ts
+++ src/buildParams.ts
@@ -3,7 +3,7 @@ export const isProd =
   process.env.REACT_APP_ENV === "production";
 
 // Change this value to disable Redux logging in development
-export const enableReduxLogging = false;
+export const enableReduxLogging = !isProd && true;
 
 export const baseUrls: Record<string, string> = {
   newm: isProd ? "https://studio.newm.io/" : "https://garage.newm.io/",
