d5c60f4074c843bdbb93fd5abb26a119509fc861
escobarjonatan <escobarjonatan11@gmail.com>
2023-05-22 14:45:57 -0500

    Updating edit song mint to match new mocks

--- src/pages/home/library/MintSong.tsx
+++ src/pages/home/library/MintSong.tsx
@@ -1,4 +1,4 @@
-import { Box, Button as MUIButton, Stack } from "@mui/material";
+import { AlertTitle, Box, Button as MUIButton, Stack } from "@mui/material";
 import { useLocation, useNavigate } from "react-router";
 import { useWindowDimensions } from "common";
 import { Alert, Button, HorizontalLine, Typography } from "elements";
@@ -157,13 +157,18 @@ const MintSong = () => {
                   setShowWarning(false);
                 } }
                 sx={ { textTransform: "none" } }
+                variant={ "outlined" }
               >
                 Dismiss
               </MUIButton>
             }
           >
-            <Typography color="baseBlue" fontWeight={ 500 } variant="subtitle1">
+            <AlertTitle sx={ { color: theme.colors.baseBlue, fontWeight: 600 } }>
               These details cannot be changed after minting.
+            </AlertTitle>
+            <Typography color="baseBlue" fontWeight={ 500 } variant="subtitle1">
+              Please review all details carefully before moving forward with the
+              minting process.
             </Typography>
           </Alert>
         </Box>
