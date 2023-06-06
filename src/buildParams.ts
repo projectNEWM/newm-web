5c3d7822762bccf7f4e61dc5ddced6fdc034ddc7
escobarjonatan <escobarjonatan11@gmail.com>
2023-06-06 09:29:39 -0500

    Removed extra path

--- src/buildParams.ts
+++ src/buildParams.ts
@@ -9,6 +9,6 @@ export const baseUrls: Record<string, string> = {
   newm: isProd ? "https://studio.newm.io/" : "https://garage.newm.io/",
   cloudinary: "https://api.cloudinary.com/",
   lambda: isProd
-    ? "https://aws.studio.newm.io/generate-artist-agreement"
-    : "https://aws.garage.newm.io/generate-artist-agreement",
+    ? "https://aws.studio.newm.io/"
+    : "https://aws.garage.newm.io/",
 };
