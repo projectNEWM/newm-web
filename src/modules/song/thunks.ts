e66d64725205edd15b3d2e848a8476b841668728
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2023-06-10 21:27:34 -0700

    minor updates to upload and patch song thunks

--- src/modules/song/thunks.ts
+++ src/modules/song/thunks.ts
@@ -87,6 +87,19 @@ export const uploadSong = createAsyncThunk(
         body: formData,
       });
 
+      const generateArtistAgreementResponse = await dispatch(
+        generateArtistAgreement({
+          artistName: body.artistName,
+          companyName: body.companyName,
+          saved: true,
+          songId,
+          songName: body.title,
+          stageName: body.stageName,
+        })
+      );
+
+      if ("error" in generateArtistAgreementResponse) return;
+
       if (body.isMinting) {
         const collaborators = generateCollaborators(
           body.owners,
@@ -113,19 +126,6 @@ export const uploadSong = createAsyncThunk(
         for (const collabResp of collabResponses) {
           if ("error" in collabResp) return;
         }
-
-        const generateArtistAgreementResponse = await dispatch(
-          generateArtistAgreement({
-            artistName: body.artistName,
-            companyName: body.companyName,
-            saved: true,
-            songId,
-            songName: body.title,
-            stageName: body.stageName,
-          })
-        );
-
-        if ("error" in generateArtistAgreementResponse) return;
 
         const processStreamTokenAgreementResponse = await dispatch(
           songApi.endpoints.processStreamTokenAgreement.initiate({
@@ -294,25 +294,13 @@ export const patchSong = createAsyncThunk(
           if ("error" in collabResp) return;
         }
 
-        if (body.title && body.artistName) {
-          await dispatch(
-            generateArtistAgreement({
-              artistName: body.artistName,
-              companyName: body.companyName,
-              saved: true,
-              songId: body.id,
-              songName: body.title,
-              stageName: body.stageName,
-            })
-          );
-        }
-
         const songResp = await dispatch(
           songApi.endpoints.getSong.initiate(body.id)
         );
 
         if ("error" in songResp || !songResp.data) return;
 
+        // approve minting agreement if hasn't been approved yet
         if (
           body.consentsToContract &&
           songResp.data.mintingStatus === MintingStatus.Undistributed
