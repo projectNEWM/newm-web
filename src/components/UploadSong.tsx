6b82bec4afa5cf7ff5d70afaecf4a107999fdbf2
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2023-06-18 23:45:20 -0700

    updates upload song component to match mocks

--- src/components/UploadSong.tsx
+++ src/components/UploadSong.tsx
@@ -1,9 +1,22 @@
 import { Box, Stack } from "@mui/material";
-import { FunctionComponent, useCallback, useState } from "react";
+import {
+  FunctionComponent,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { FileRejection, useDropzone } from "react-dropzone";
 import { useTheme } from "@mui/material/styles";
 import AddSongIcon from "assets/images/AddSong";
 import CheckCircleIcon from "assets/images/CheckCircle";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import StopIcon from "@mui/icons-material/Stop";
+import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
+import { AudioVisualizer } from "react-audio-visualize";
+import { Howl } from "howler";
+import { getFileBinary } from "common";
 import IconMessage from "./IconMessage";
 import DashedOutline from "./styled/DashedOutline";
 import ErrorMessage from "./styled/ErrorMessage";
@@ -15,6 +28,11 @@ interface UploadSongProps {
   readonly onError: (message: string) => void;
   readonly onBlur: VoidFunction;
   readonly errorMessage?: string;
+}
+
+interface SongProgressOverlayProps {
+  readonly progress: string;
+  readonly isPlaying: boolean;
 }
 
 /**
@@ -31,6 +49,25 @@ const UploadSong: FunctionComponent<UploadSongProps> = ({
   const theme = useTheme();
 
   const [isHovering, setIsHovering] = useState(false);
+  const [song, setSong] = useState<Howl | null>(null);
+  const [songProgress, setSongProgress] = useState<string>("0%");
+  const visualizerRef = useRef<HTMLCanvasElement>(null);
+
+  const isSongPlaying = !!song?.playing();
+
+  const handlePlaySong: MouseEventHandler = (event) => {
+    event.stopPropagation();
+    if (!song) return;
+
+    song.play();
+  };
+
+  const handleStopSong: MouseEventHandler = (event) => {
+    event.stopPropagation();
+    if (!song) return;
+
+    song.stop();
+  };
 
   const handleDrop = useCallback(
     async (
@@ -67,6 +104,45 @@ const UploadSong: FunctionComponent<UploadSongProps> = ({
     },
   });
 
+  /**
+   * Set playable audio when file is uploaded or changed.
+   */
+  useEffect(() => {
+    const setBase64AudioString = async () => {
+      if (!file) return;
+
+      const binary = (await getFileBinary(file)) as string;
+      const howler = new Howl({ src: binary });
+      setSong(howler);
+    };
+
+    setBase64AudioString();
+  }, [file]);
+
+  /**
+   * Update song progress each second.
+   */
+  useEffect(() => {
+    const getSongProgress = () => {
+      if (!song) {
+        return "0%";
+      }
+
+      const songDuration = song.duration();
+      const songPosition = song.seek();
+      const songProgress = songPosition / songDuration;
+
+      return songProgress * 100 + "%";
+    };
+
+    const interval = setInterval(() => {
+      const songProgress = getSongProgress();
+      setSongProgress(songProgress);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [song]);
+
   return (
     <Stack direction="column" spacing={ 1 } alignItems="center">
       <Box
@@ -94,22 +170,98 @@ const UploadSong: FunctionComponent<UploadSongProps> = ({
               flexGrow: 1,
             } }
           >
-            { isHovering || isDragActive ? (
-              <IconMessage
-                icon={ <AddSongIcon /> }
-                message="Upload a new song"
-                subtitle=".wav or .flac"
+            <Box position="relative">
+              <AudioVisualizer
+                ref={ visualizerRef }
+                blob={ file }
+                width={ 338 }
+                height={ 80 }
+                barWidth={ 1 }
+                gap={ 0 }
+                barColor={ theme.colors.grey100 }
+                style={ { opacity: 0.2, marginLeft: 2 } }
               />
-            ) : (
-              <IconMessage icon={ <CheckCircleIcon /> } message={ file.name } />
-            ) }
+
+              <Box
+                position="absolute"
+                display="flex"
+                top={ 0 }
+                right={ 0 }
+                bottom={ 0 }
+                left={ 0 }
+                width="100%"
+                height="100%"
+                justifyContent="center"
+                alignItems="center"
+                zIndex={ 10 }
+              >
+                <SongProgressOverlay
+                  progress={ songProgress }
+                  isPlaying={ isSongPlaying }
+                />
+
+                { isHovering ? (
+                  <Box display="flex" justifyContent="space-between" flex={ 1 }>
+                    <Box
+                      display="flex"
+                      flex={ 1 }
+                      justifyContent="center"
+                      alignItems="center"
+                      sx={ {
+                        "&:hover": {
+                          backgroundColor: "rgba(0, 0, 0, 0.35)",
+                        },
+                      } }
+                    >
+                      <IconMessage
+                        icon={ <ChangeCircleIcon /> }
+                        message="Change song"
+                      />
+                    </Box>
+
+                    <Box
+                      display="flex"
+                      flex={ 1 }
+                      justifyContent="center"
+                      alignItems="center"
+                      sx={ {
+                        "&:hover": {
+                          backgroundColor: "rgba(0, 0, 0, 0.35)",
+                        },
+                      } }
+                    >
+                      { isSongPlaying ? (
+                        <IconMessage
+                          icon={ <StopIcon /> }
+                          message="Stop song"
+                          onClick={ handleStopSong }
+                        />
+                      ) : (
+                        <IconMessage
+                          icon={ <PlayArrowIcon /> }
+                          message="Play song"
+                          onClick={ handlePlaySong }
+                        />
+                      ) }
+                    </Box>
+                  </Box>
+                ) : isDragActive ? (
+                  <IconMessage
+                    icon={ <ChangeCircleIcon /> }
+                    message="Drop your song here"
+                  />
+                ) : (
+                  <IconMessage icon={ <CheckCircleIcon /> } message={ file.name } />
+                ) }
+              </Box>
+            </Box>
           </SolidOutline>
         ) : (
           <DashedOutline sx={ { display: "flex", flexGrow: 1 } }>
             <IconMessage
               icon={ <AddSongIcon /> }
               message="Drag and drop or browse your song"
-              subtitle=".wav or .flac"
+              subtitle=".flac, .fla, .wav"
             />
           </DashedOutline>
         ) }
@@ -119,6 +271,30 @@ const UploadSong: FunctionComponent<UploadSongProps> = ({
         <ErrorMessage align="center">{ errorMessage }</ErrorMessage>
       ) }
     </Stack>
+  );
+};
+
+const SongProgressOverlay: FunctionComponent<SongProgressOverlayProps> = ({
+  progress,
+  isPlaying,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      position="absolute"
+      top={ 0 }
+      bottom={ 0 }
+      left={ 0 }
+      width={ progress }
+      marginLeft="2px"
+      zIndex={ -10 }
+      sx={ {
+        opacity: 0.5,
+        backgroundColor: theme.colors.black,
+        transition: isPlaying ? "width 1s linear" : "width 0",
+      } }
+    />
   );
 };
 
