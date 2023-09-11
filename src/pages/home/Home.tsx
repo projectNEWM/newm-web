9c33185a7795b0f71428f6b171e787a70d4f7943
escobarjonatan <escobarjonatan11@gmail.com>
2023-09-11 11:46:15 -0500

    fix: Added logic to handle async redirect glitch

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,9 @@
 import { Box, IconButton } from "@mui/material";
 import { FunctionComponent, useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import MenuIcon from "@mui/icons-material/Menu";
+import { emptyProfile, useGetProfileQuery } from "modules/session";
 import SideBar from "./SideBar";
 import UploadSong from "./uploadSong/UploadSong";
 import Library from "./library/Library";
@@ -13,10 +14,22 @@ import Settings from "./settings/Settings";
 
 const Home: FunctionComponent = () => {
   const drawerWidth = 230;
-
   const theme = useTheme();
+  const navigate = useNavigate();
 
   const [isMobileOpen, setMobileOpen] = useState(false);
+  const [isRedirectInProgress, setIsRedirectInProgress] = useState(false);
+
+  const {
+    data: { firstName = "", lastName = "", role } = emptyProfile,
+    isLoading,
+  } = useGetProfileQuery();
+
+  if (!isLoading && (!firstName || !lastName || !role)) {
+    if (!isRedirectInProgress) setIsRedirectInProgress(true);
+
+    navigate("/create-profile");
+  }
 
   return (
     <Box
@@ -26,34 +39,38 @@ const Home: FunctionComponent = () => {
         flexGrow: 1,
       } }
     >
-      <SideBar isMobileOpen={ isMobileOpen } setMobileOpen={ setMobileOpen } />
+      { isLoading || isRedirectInProgress ? null : (
+        <>
+          <SideBar isMobileOpen={ isMobileOpen } setMobileOpen={ setMobileOpen } />
 
-      <Box
-        component="main"
-        sx={ {
-          flexGrow: 1,
-          paddingY: 10.5,
-          paddingX: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          marginLeft: { md: 30 },
-        } }
-      >
-        <Box position="absolute" left="2rem" top="2rem">
-          <IconButton onClick={ () => setMobileOpen(true) }>
-            <MenuIcon sx={ { color: "white" } } />
-          </IconButton>
-        </Box>
-        <Routes>
-          <Route path="" element={ <Navigate to="upload-song" replace /> } />
+          <Box
+            component="main"
+            sx={ {
+              flexGrow: 1,
+              paddingY: 10.5,
+              paddingX: 3,
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+              marginLeft: { md: 30 },
+            } }
+          >
+            <Box position="absolute" left="2rem" top="2rem">
+              <IconButton onClick={ () => setMobileOpen(true) }>
+                <MenuIcon sx={ { color: "white" } } />
+              </IconButton>
+            </Box>
+            <Routes>
+              <Route path="" element={ <Navigate to="upload-song" replace /> } />
 
-          <Route path="upload-song/*" element={ <UploadSong /> } />
-          <Route path="library/*" element={ <Library /> } />
-          <Route path="collaborators/*" element={ <Owners /> } />
-          <Route path="wallet" element={ <Wallet /> } />
-          <Route path="profile" element={ <Profile /> } />
-          <Route path="settings" element={ <Settings /> } />
-        </Routes>
-      </Box>
+              <Route path="upload-song/*" element={ <UploadSong /> } />
+              <Route path="library/*" element={ <Library /> } />
+              <Route path="collaborators/*" element={ <Owners /> } />
+              <Route path="wallet" element={ <Wallet /> } />
+              <Route path="profile" element={ <Profile /> } />
+              <Route path="settings" element={ <Settings /> } />
+            </Routes>
+          </Box>
+        </>
+      ) }
     </Box>
   );
 };
