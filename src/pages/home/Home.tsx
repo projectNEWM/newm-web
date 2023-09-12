1b4955da1a971db6c6c91269933d98fd4cd1d851
escobarjonatan <escobarjonatan11@gmail.com>
2023-09-12 00:47:15 -0500

    fix: Removing state and return early when processing

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -18,18 +18,21 @@ const Home: FunctionComponent = () => {
   const navigate = useNavigate();
 
   const [isMobileOpen, setMobileOpen] = useState(false);
-  const [isRedirectInProgress, setIsRedirectInProgress] = useState(false);
 
   const {
     data: { firstName = "", lastName = "", role } = emptyProfile,
     isLoading,
   } = useGetProfileQuery();
 
-  if (!isLoading && (!firstName || !lastName || !role)) {
-    if (!isRedirectInProgress) setIsRedirectInProgress(true);
+  const hasBasicDetails = !!(firstName && lastName && role);
 
+  if (!hasBasicDetails && !isLoading) {
     navigate("/create-profile");
+
+    return null;
   }
+
+  if (isLoading) return null;
 
   return (
     <Box
@@ -39,38 +42,34 @@ const Home: FunctionComponent = () => {
         flexGrow: 1,
       } }
     >
-      { isLoading || isRedirectInProgress ? null : (
-        <>
-          <SideBar isMobileOpen={ isMobileOpen } setMobileOpen={ setMobileOpen } />
+      <SideBar isMobileOpen={ isMobileOpen } setMobileOpen={ setMobileOpen } />
 
-          <Box
-            component="main"
-            sx={ {
-              flexGrow: 1,
-              paddingY: 10.5,
-              paddingX: 3,
-              width: { sm: `calc(100% - ${drawerWidth}px)` },
-              marginLeft: { md: 30 },
-            } }
-          >
-            <Box position="absolute" left="2rem" top="2rem">
-              <IconButton onClick={ () => setMobileOpen(true) }>
-                <MenuIcon sx={ { color: "white" } } />
-              </IconButton>
-            </Box>
-            <Routes>
-              <Route path="" element={ <Navigate to="upload-song" replace /> } />
+      <Box
+        component="main"
+        sx={ {
+          flexGrow: 1,
+          paddingY: 10.5,
+          paddingX: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          marginLeft: { md: 30 },
+        } }
+      >
+        <Box position="absolute" left="2rem" top="2rem">
+          <IconButton onClick={ () => setMobileOpen(true) }>
+            <MenuIcon sx={ { color: "white" } } />
+          </IconButton>
+        </Box>
+        <Routes>
+          <Route path="" element={ <Navigate to="upload-song" replace /> } />
 
-              <Route path="upload-song/*" element={ <UploadSong /> } />
-              <Route path="library/*" element={ <Library /> } />
-              <Route path="collaborators/*" element={ <Owners /> } />
-              <Route path="wallet" element={ <Wallet /> } />
-              <Route path="profile" element={ <Profile /> } />
-              <Route path="settings" element={ <Settings /> } />
-            </Routes>
-          </Box>
-        </>
-      ) }
+          <Route path="upload-song/*" element={ <UploadSong /> } />
+          <Route path="library/*" element={ <Library /> } />
+          <Route path="collaborators/*" element={ <Owners /> } />
+          <Route path="wallet" element={ <Wallet /> } />
+          <Route path="profile" element={ <Profile /> } />
+          <Route path="settings" element={ <Settings /> } />
+        </Routes>
+      </Box>
     </Box>
   );
 };
