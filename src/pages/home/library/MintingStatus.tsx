555e79669996b25350d502c42df29bc5684db8c3
escobarjonatan <escobarjonatan11@gmail.com>
2023-09-15 22:53:38 -0500

    Add new minting status and refactor status icon configuration

--- src/pages/home/library/MintingStatus.tsx
+++ src/pages/home/library/MintingStatus.tsx
@@ -1,72 +1,85 @@
-import { Check, Close } from "@mui/icons-material";
-import InfoCircleLine from "assets/images/InfoCircleLine";
-import TimeCircleLine from "assets/images/TimeCircleLine";
 import { MintingStatus as MintingStatusType } from "modules/song";
 import { IconStatus } from "components";
 import { FunctionComponent } from "react";
+import { Theme } from "@mui/material";
+import { Check, Close } from "@mui/icons-material";
+import InfoCircleLine from "assets/images/InfoCircleLine";
+import TimeCircleLine from "assets/images/TimeCircleLine";
 import theme from "theme";
 
 interface MintingStatusProps {
   readonly mintingStatus: MintingStatusType;
 }
 
+const UI_MINTING_STATUS: Record<MintingStatusType, string> = {
+  Undistributed: "Undistributed",
+  StreamTokenAgreementApproved: "Undistributed",
+  MintingPaymentRequested: "Undistributed",
+  MintingPaymentSubmitted: "Undistributed",
+  MintingPaymentReceived: "Undistributed",
+  AwaitingAudioEncoding: "Undistributed",
+  AwaitingCollaboratorApproval: "Awaiting Collaborator Approval",
+  ReadyToDistribute: "Distribution/Minting in Process",
+  SubmittedForDistribution: "Distribution/Minting in Process",
+  Distributed: "Distribution/Minting in Process",
+  Pending: "Distribution/Minting in Process",
+  Declined: "Declined",
+  Minted: "Distributed & Minted",
+  MintingPaymentTimeout: "An error occurred",
+  MintingPaymentException: "An error occurred",
+  DistributionException: "An error occurred",
+  SubmittedForDistributionException: "An error occurred",
+  ArweaveUploadException: "An error occurred",
+  MintingException: "An error occurred",
+};
+
+const STATUS_ICON_CONFIG: Record<
+  string,
+  {
+    icon: JSX.Element;
+    color?: keyof Theme["colors"];
+    fontColor?: keyof Theme["colors"];
+  }
+> = {
+  Undistributed: {
+    icon: <Close fontSize="medium" sx={ { color: theme.colors.grey200 } } />,
+    fontColor: "grey200",
+  },
+  "Awaiting Collaborator Approval": {
+    icon: <TimeCircleLine />,
+    color: "yellow",
+  },
+  "Distribution/Minting in Process": {
+    icon: <TimeCircleLine />,
+    color: "yellow",
+  },
+  Declined: {
+    icon: <InfoCircleLine />,
+    color: "red",
+  },
+  "Distributed & Minted": {
+    icon: <Check fontSize="medium" sx={ { color: theme.colors.green } } />,
+  },
+  "An error occurred": {
+    icon: <InfoCircleLine />,
+    color: "red",
+  },
+};
+
 export const MintingStatus: FunctionComponent<MintingStatusProps> = ({
   mintingStatus,
 }) => {
-  const isPending = [
-    MintingStatusType.StreamTokenAgreementApproved,
-    MintingStatusType.MintingPaymentRequested,
-    MintingStatusType.MintingPaymentReceived,
-    MintingStatusType.ReadyToDistribute,
-    MintingStatusType.SubmittedForDistribution,
-    MintingStatusType.Distributed,
-    MintingStatusType.Pending,
-  ].includes(mintingStatus);
+  const status = UI_MINTING_STATUS[mintingStatus];
+  const config = STATUS_ICON_CONFIG[status];
 
-  if (mintingStatus === MintingStatusType.Distributed) {
-    return (
-      <IconStatus
-        icon={ <Check fontSize="medium" sx={ { color: theme.colors.green } } /> }
-        status="Distributed"
-      />
-    );
-  } else if (isPending) {
-    return (
-      <IconStatus
-        icon={ <TimeCircleLine /> }
-        iconColor="yellow"
-        status="Pending"
-      />
-    );
-  } else if (mintingStatus === MintingStatusType.AwaitingCollaboratorApproval) {
-    return (
-      <IconStatus
-        icon={ <TimeCircleLine /> }
-        iconColor="yellow"
-        status="Pending Collaborator Approval"
-      />
-    );
-  } else if (mintingStatus === MintingStatusType.Declined) {
-    return (
-      <IconStatus icon={ <InfoCircleLine /> } iconColor="red" status="Declined" />
-    );
-  } else if (mintingStatus === MintingStatusType.MintingPaymentSubmitted) {
-    return (
-      <IconStatus
-        icon={ <TimeCircleLine /> }
-        iconColor="yellow"
-        status="Minting Payment Submitted"
-      />
-    );
-  } else if (mintingStatus === MintingStatusType.Undistributed) {
-    return (
-      <IconStatus
-        icon={ <Close fontSize="medium" sx={ { color: theme.colors.grey200 } } /> }
-        fontColor="grey200"
-        status="Undistributed"
-      />
-    );
-  } else {
-    return null;
-  }
+  if (!config) return null;
+
+  return (
+    <IconStatus
+      icon={ config.icon }
+      iconColor={ config.color }
+      fontColor={ config.fontColor }
+      status={ status }
+    />
+  );
 };
