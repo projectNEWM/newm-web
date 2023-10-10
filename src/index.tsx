36d844f9e01a8e28e8e0a3ebb3b6f501b56ed093
escobarjonatan <escobarjonatan11@gmail.com>
2023-10-10 00:11:50 -0500

    feat: Added Sentry for error logging purposes

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import initializeSentry from "./sentryConfig";
+
+initializeSentry();
 
 const isMac = navigator.userAgent.includes("Mac");
 
