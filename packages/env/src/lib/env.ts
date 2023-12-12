84db2cdf02480e5b79b745efc8d6aab56d7182b9
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2023-12-11 20:57:32 -0800

    updates env NODE_ENV export

--- packages/env/src/lib/env.ts
+++ packages/env/src/lib/env.ts
@@ -2,4 +2,4 @@ export const VITE_FACEBOOK_CLIENT_ID = import.meta.env.VITE_FACEBOOK_CLIENT_ID;
 export const VITE_GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 export const VITE_LINKEDIN_CLIENT_ID = import.meta.env.VITE_LINKEDIN_CLIENT_ID;
 export const VITE_ENV = import.meta.env.VITE_ENV;
-export const NODE_ENV = import.meta.env.NODE_ENV;
+export const NODE_ENV = process.env.NODE_ENV;
