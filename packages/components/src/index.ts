d563dd457c94f3d2eb76232f28b0f879dda60282
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-02-05 22:57:09 -0800

    orders component package exports alphabetically

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,6 @@
-export * from "./lib/modals/WalletEnvMismatchModal";
-export { default as WalletEnvMismatchModal } from "./lib/modals/WalletEnvMismatchModal";
 export * from "./lib/buttons/DisconnectWalletButton";
 export { default as DisconnectWalletButton } from "./lib/buttons/DisconnectWalletButton";
+export * from "./lib/modals/WalletEnvMismatchModal";
+export { default as WalletEnvMismatchModal } from "./lib/modals/WalletEnvMismatchModal";
 export * from "./lib/modals/WalletModal";
 export { default as WalletModal } from "./lib/modals/WalletModal";
