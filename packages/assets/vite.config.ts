bf309bd323a0c5fa2e8ece5eeda5c06872fc6974
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-02-12 21:41:54 -0800

    migrates nx to v18

--- packages/assets/vite.config.ts
+++ packages/assets/vite.config.ts
@@ -6,6 +6,7 @@ import dts from "vite-plugin-dts";
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
+  root: __dirname,
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
@@ -13,6 +14,9 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: "../../dist/packages/assets",
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: "src/index.ts",
