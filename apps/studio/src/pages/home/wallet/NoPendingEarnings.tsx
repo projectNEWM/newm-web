2c760aee9d05e5b7a5bd12bebe673b6b34def273
escobarjonatan <escobarjonatan11@gmail.com>
2024-02-21 22:04:43 -0600

    feat: STUD-96 Add UI for no pending earnings in wallet (#535)
    * feat: STUD-96 Add UI for no pending earnings in wallet
    
    * feat: STUD-96 Update logic to display/hide earning state

---
+++ apps/studio/src/pages/home/wallet/NoPendingEarnings.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from "react";
+import { Stack, Typography } from "@mui/material";
+import { CheckCircle } from "@newm-web/assets";
+import theme from "@newm-web/theme";
+
+export const NoPendingEarnings: FunctionComponent = () => (
+  <Stack
+    sx={ {
+      backgroundColor: theme.colors.grey600,
+      borderRadius: "6px",
+      flexDirection: "row",
+      gap: 1.5,
+      p: 2,
+      width: "fit-content",
+    } }
+  >
+    <CheckCircle fill={ theme.colors.green } />
+    <Stack>
+      <Typography color={ theme.colors.green } fontWeight={ 500 }>
+        No pending earnings to claim
+      </Typography>
+      <Typography color={ theme.colors.grey200 } fontWeight={ 500 }>
+        Total earnings accrued so far: ##.##Ɲ (~$#.##)
+      </Typography>
+    </Stack>
+  </Stack>
+);
