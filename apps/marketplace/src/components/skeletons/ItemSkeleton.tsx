6c361c30144d09bc7f17e123aa634c0615a7c086
dmkirshon <kirshon.david@gmail.com>
2024-04-09 18:07:18 +0200

    feat: Add item description skeleton

--- apps/marketplace/src/components/skeletons/ItemSkeleton.tsx
+++ apps/marketplace/src/components/skeletons/ItemSkeleton.tsx
@@ -3,13 +3,22 @@ import { FunctionComponent } from "react";
 
 const ItemSkeleton: FunctionComponent = () => (
   <Container maxWidth="md" sx={ { flexGrow: 1, mt: 5 } }>
-    <Stack mt={ 2.5 }>
-      <Box height={ 400 } p={ 1.5 } width={ 400 }>
+    <Stack direction={ "row" } mt={ 2.5 }>
+      <Box height={ 400 } mr={ 5 } p={ 1.5 } width={ 400 }>
         <Skeleton
           sx={ { height: "100%", width: "100%" } }
           variant="rectangular"
         />
       </Box>
+      <Stack height={ 440 } p={ 1.5 } width={ 440 }>
+        <Skeleton height={ 38 } width={ 120 } />
+        <Skeleton height={ 16 } width={ 60 } />
+        <Skeleton sx={ { height: "100%", width: "100%" } } variant="text" />
+        <Stack alignItems={ "center" } direction={ "row" } gap={ 1.5 }>
+          <Skeleton height={ 40 } variant="circular" width={ 40 } />
+          <Skeleton height={ 20 } width={ 120 } />
+        </Stack>
+      </Stack>
     </Stack>
   </Container>
 );
