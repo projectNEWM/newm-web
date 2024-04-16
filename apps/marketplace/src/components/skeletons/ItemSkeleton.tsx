ccdc958ba2040e2146c5b47fe420bf32a10af69c
David Kirshon <86050631+dmkirshon@users.noreply.github.com>
2024-04-16 09:46:08 +0200

    MRKT-31-Single-Item-Description (#590)
    * feat: Update test data for item description
    
    * feat: Add item description skeleton
    
    * feat: Add item description with mock data
    
    * doc: Remove unnecessary comment in item page
    
    * refactor: Remove redundant display flex on stack

--- apps/marketplace/src/components/skeletons/ItemSkeleton.tsx
+++ apps/marketplace/src/components/skeletons/ItemSkeleton.tsx
@@ -2,14 +2,44 @@ import { Box, Container, Skeleton, Stack } from "@mui/material";
 import { FunctionComponent } from "react";
 
 const ItemSkeleton: FunctionComponent = () => (
-  <Container maxWidth="md" sx={ { flexGrow: 1, mt: 5 } }>
-    <Stack mt={ 2.5 }>
-      <Box height={ 400 } p={ 1.5 } width={ 400 }>
+  <Container maxWidth="md" sx={ { flexGrow: 1, mt: [0, 0, 5] } }>
+    <Stack
+      alignItems={ ["center", "center", "start"] }
+      direction={ ["column", "column", "row"] }
+    >
+      <Box
+        height={ [240, 240, 400] }
+        mb={ [2, 2, 0] }
+        mr={ [0, 0, 5] }
+        p={ 1.5 }
+        width={ [240, 240, 400] }
+      >
         <Skeleton
           sx={ { height: "100%", width: "100%" } }
           variant="rectangular"
         />
       </Box>
+      <Stack
+        alignItems={ ["center", "center", "start"] }
+        gap={ [4, 4, 2.5] }
+        pt={ [0, 0, 1.5] }
+        width={ ["100%", 440, 440] }
+      >
+        <Stack alignItems={ ["center", "center", "start"] } gap={ 0.5 }>
+          <Skeleton height={ 38 } width={ 120 } />
+          <Skeleton height={ 16 } width={ 60 } />
+        </Stack>
+        <Skeleton height={ 88 } variant="text" width={ "100%" } />
+        <Stack
+          alignItems={ "center" }
+          direction={ "row" }
+          gap={ 1.5 }
+          justifyContent={ ["center", "center", "start"] }
+        >
+          <Skeleton height={ 40 } variant="circular" width={ 40 } />
+          <Skeleton height={ 20 } width={ 120 } />
+        </Stack>
+      </Stack>
     </Stack>
   </Container>
 );
--- apps/marketplace/src/temp/data.ts
+++ apps/marketplace/src/temp/data.ts
@@ -36,80 +36,144 @@ export const mockArtists = Array(10)
 
 export const mockSongs = [
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1706033133/efpgmcjwk8glctlwfzm8.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df58",
+    isExplicit: true,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1706033133/efpgmcjwk8glctlwfzm8.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df581",
+    isExplicit: false,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1706033133/efpgmcjwk8glctlwfzm8.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df582",
+    isExplicit: true,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1699544008/xrcmyar9m09mk3l9mo1o.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df583",
+    isExplicit: false,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1699580048/aw7w0kielduse0z4vavi.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df584",
+    isExplicit: true,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1701892098/rka1mlzzad6ohrcfqef3.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df585",
+    isExplicit: false,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1702264297/ql6f3j5tettsbc3moea3.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df586",
+    isExplicit: true,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
     title: "Vibrate Punk",
   },
   {
+    artist: mockArtist,
     coverArtUrl:
       "https://res.cloudinary.com/newm/image/upload/c_limit,w_4000,h_4000/v1702264297/ql6f3j5tettsbc3moea3.png",
+    description: `
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis 
+    natoque penatibus et magnis dis parturient montes, nascetur 
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium.`,
     genres: ["Punk"],
     id: "3cfb2d02-a320-4385-96d1-1498d8a1df587",
+    isExplicit: false,
     price: "3.0",
     streamUrl:
       "https://media.garage.newm.io/3cfb2d02-a320-4385-96d1-1498d8a1df58/audio/HLS/audio_output.m3u8",
