1ee56934941135a52d1a9a39b679ee814fb507a0
dmkirshon <kirshon.david@gmail.com>
2024-04-24 07:54:22 +0200

    feat: Add item buy function UI

--- apps/marketplace/src/app/item/[songId]/page.tsx
+++ apps/marketplace/src/app/item/[songId]/page.tsx
@@ -1,9 +1,23 @@
 "use client";
-import { Box, Container, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
 import { SongCard } from "@newm-web/components";
 import { FunctionComponent, useEffect, useState } from "react";
 import { resizeCloudinaryImage } from "@newm-web/utils";
-import { ProfileImage } from "@newm-web/elements";
+import {
+  Button,
+  ProfileImage,
+  TextInputField,
+  Tooltip,
+} from "@newm-web/elements";
+import { Form, Formik } from "formik";
 import { mockSongs } from "../../../temp/data";
 import { ItemSkeleton } from "../../../components";
 
@@ -14,6 +28,7 @@ interface SingleSongProps {
 }
 
 const SingleSong: FunctionComponent<SingleSongProps> = ({ params }) => {
+  const theme = useTheme();
   const [isLoading, setIsLoading] = useState(true);
   const songData = mockSongs.find((song) => song.id === params.songId);
 
@@ -56,19 +71,16 @@ const SingleSong: FunctionComponent<SingleSongProps> = ({ params }) => {
         >
           <Stack gap={ 0.5 }>
             <Typography variant="h3">{ songData?.title }</Typography>
-            <Typography
-              color={ (theme) => theme.colors.grey300 }
-              variant="subtitle2"
-            >
+            <Typography color={ theme.colors.grey300 } variant="subtitle2">
               { songData?.isExplicit ? "Explicit" : null }
             </Typography>
           </Stack>
-          <Typography textAlign={ "left" } variant="subtitle1">
+          <Typography textAlign="left" variant="subtitle1">
             { songData?.description }
           </Typography>
           <Stack
-            alignItems={ "center" }
-            direction={ "row" }
+            alignItems="center"
+            direction="row"
             gap={ 1.5 }
             justifyContent={ ["center", "center", "start"] }
           >
@@ -84,6 +96,93 @@ const SingleSong: FunctionComponent<SingleSongProps> = ({ params }) => {
               { songData?.artist.firstName } { songData?.artist.lastName }
             </Typography>
           </Stack>
+
+          <Formik
+            initialValues={ { streamTokens: 1 } }
+            onSubmit={ () => {
+              return;
+            } }
+          >
+            { () => {
+              return (
+                <Form>
+                  <Stack gap={ 2.5 } mb={ 4 } mt={ 0.5 }>
+                    <Stack>
+                      <Box alignSelf="flex-end" p={ 0.5 }>
+                        <Tooltip
+                          title={
+                            "The number of Stream Tokens correlates directly " +
+                            "with the percentage of Streaming royalties you " +
+                            "can acquire and the total price of the bundle. " +
+                            "For example 1 token is worth = 0.0000001% of " +
+                            "total royalties, and costs ‘Ɲ3.0‘."
+                          }
+                        >
+                          <IconButton sx={ { padding: 0 } }>
+                            <InfoIcon sx={ { color: theme.colors.grey100 } } />
+                          </IconButton>
+                        </Tooltip>
+                      </Box>
+
+                      <Box
+                        borderRadius={ 2 }
+                        px={ 3 }
+                        py={ 2.5 }
+                        sx={ { backgroundColor: theme.colors.grey600 } }
+                      >
+                        <Stack>
+                          <Typography
+                            fontWeight={ theme.typography.fontWeightBold }
+                            variant="subtitle2"
+                          >
+                            STREAM TOKENS
+                          </Typography>
+                          <Stack direction={ "row" }>
+                            <Box maxWidth={ "150px" }>
+                              <TextInputField name="streamTokens"></TextInputField>
+                            </Box>
+                            <Typography
+                              alignSelf="center"
+                              flexShrink={ "0" }
+                              pl={ 1.5 }
+                              variant="subtitle2"
+                            >
+                              = 0.0000001% of total royalties
+                            </Typography>
+                          </Stack>
+                          <Typography
+                            color={ theme.colors.grey300 }
+                            pt={ 0.5 }
+                            variant="subtitle2"
+                          >
+                            Maximum stream tokens = 80000
+                          </Typography>
+                        </Stack>
+                      </Box>
+                    </Stack>
+
+                    <Box
+                      borderRadius={ 2 }
+                      px={ 3 }
+                      py={ 2.5 }
+                      sx={ {
+                        backgroundColor: theme.colors.grey600,
+                      } }
+                    >
+                      <Button type="submit" width="full">
+                        <Typography
+                          fontWeight={ theme.typography.fontWeightBold }
+                          variant="body1"
+                        >
+                          Buy 1 Stream Token • { "4.73N (~ $7.30)" }
+                        </Typography>
+                      </Button>
+                    </Box>
+                  </Stack>
+                </Form>
+              );
+            } }
+          </Formik>
         </Stack>
       </Stack>
     </Container>
