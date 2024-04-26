c2cf4bc392c70ee3051d2b2e7cc71c5bc85e465f
David Kirshon <86050631+dmkirshon@users.noreply.github.com>
2024-04-26 07:31:55 +0200

    MRKT-32-Single-Item-Buy-Function (#602)
    * feat: Add skeleton for item buy function
    
    * feat: Add item buy function UI
    
    * feat: Update Item skeleton to match buy UI
    
    * feat: Adjust item page mobile view for buy UI
    
    * feat: Update tooltip icon to match figma

--- apps/marketplace/src/components/skeletons/ItemSkeleton.tsx
+++ apps/marketplace/src/components/skeletons/ItemSkeleton.tsx
@@ -14,10 +14,7 @@ const ItemSkeleton: FunctionComponent = () => (
         p={ 1.5 }
         width={ [240, 240, 400] }
       >
-        <Skeleton
-          sx={ { height: "100%", width: "100%" } }
-          variant="rectangular"
-        />
+        <Skeleton sx={ { height: "100%", width: "100%" } } variant="rounded" />
       </Box>
       <Stack
         alignItems={ ["center", "center", "start"] }
@@ -29,15 +26,23 @@ const ItemSkeleton: FunctionComponent = () => (
           <Skeleton height={ 38 } width={ 120 } />
           <Skeleton height={ 16 } width={ 60 } />
         </Stack>
-        <Skeleton height={ 88 } variant="text" width={ "100%" } />
+        <Skeleton height={ 88 } variant="text" width="100%" />
         <Stack
-          alignItems={ "center" }
-          direction={ "row" }
+          alignItems="center"
+          direction="row"
           gap={ 1.5 }
           justifyContent={ ["center", "center", "start"] }
         >
           <Skeleton height={ 40 } variant="circular" width={ 40 } />
           <Skeleton height={ 20 } width={ 120 } />
+        </Stack>
+        <Stack mt={ 4.5 } width="100%">
+          <Box>
+            <Skeleton height={ 124 } variant="rounded" width={ "100%" } />
+          </Box>
+          <Box pt={ 2.5 }>
+            <Skeleton height={ 80 } variant="rounded" width={ "100%" } />
+          </Box>
         </Stack>
       </Stack>
     </Stack>
