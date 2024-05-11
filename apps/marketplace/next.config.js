dc967f605ba5fcfbcb5078d1c9639d9b82362b63
James Ayvaz <jayvaz@newm.io>
2024-05-11 14:32:54 -0700

    address PR feedback

--- apps/marketplace/next.config.js
+++ apps/marketplace/next.config.js
@@ -10,20 +10,20 @@ const nextConfig = {
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: false
   },
 
   compiler: {
     // For other options, see https://styled-components.com/docs/tooling#babel-plugin
-    styledComponents: true,
+    styledComponents: true
   },
 
-  trailingSlash: true,
+  trailingSlash: true
 };
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
-  withNx,
+  withNx
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
