aa6b735f0ac63f886c3dad6bbacfa3ff46f92815
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-05-23 16:26:23 -0700

    Revert "temporarily references env var directly for testing (#636)"
    This reverts commit e0ca0d30b034f634e4da1641ffdd6d8d889d615c.

--- apps/mobile-wallet-connector/src/api/utils.ts
+++ apps/mobile-wallet-connector/src/api/utils.ts
@@ -1,23 +1,7 @@
 import { BaseQueryApi } from "@reduxjs/toolkit/dist/query/baseQueryTypes";
 import { AxiosRequestConfig } from "axios";
-import { ReCaptchaInstance, load } from "recaptcha-v3";
+import { executeRecaptcha } from "@newm-web/utils";
 import { recaptchaEndpointActionMap } from "./constants";
-
-let recaptcha: ReCaptchaInstance | undefined;
-
-const executeRecaptcha = async (action: string) => {
-  if (!process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING) {
-    throw new Error("Missing Recaptcha site key environment variable");
-  }
-
-  if (!recaptcha) {
-    recaptcha = await load(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING, {
-      autoHideBadge: true,
-    });
-  }
-
-  return await recaptcha.execute(action);
-};
 
 /**
  * Returns recaptcha headers for unauthenticated requests.
