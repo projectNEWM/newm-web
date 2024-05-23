3ade34fada930331732e791c93e795058f4fbc3e
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-05-22 18:19:43 -0700

    attempt to fix routing (#641)

--- apps/mobile-wallet-connector/next.config.js
+++ apps/mobile-wallet-connector/next.config.js
@@ -7,21 +7,30 @@ const { composePlugins, withNx } = require("@nx/next");
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/wallet-connect/:path*",
+        destination: "/:path*"
+      }
+    ];
+  },
+
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: false
   },
 
   compiler: {
     // For other options, see https://styled-components.com/docs/tooling#babel-plugin
-    styledComponents: true,
-  },
+    styledComponents: true
+  }
 };
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
-  withNx,
+  withNx
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
