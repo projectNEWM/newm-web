81d3f2c2aa3a6b8d4f2399b812f1bda01846242b
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-05-23 13:25:05 -0700

    reverts potentially unecessary mwc deploy changes (#650)

--- .github/workflows/cdk/bin/deploy.ts
+++ .github/workflows/cdk/bin/deploy.ts
@@ -9,6 +9,7 @@ import { Tags } from "aws-cdk-lib";
 
 const appName = process.env.APPNAME || "APPNAME";
 const appId = process.env.APPID || "APPID";
+const recaptchaKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING || "";
 const qualifier = process.env.QUALIFIER || "UNDEFINED";
 const rootDir = path.resolve(__dirname, "..", "..", "..", "..");
 
@@ -16,17 +17,11 @@ class WebDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const recaptchaKey =
-      process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING || "";
-
     const deployFunction = new lambda.DockerImageFunction(
       this,
       "DeployFunction",
       {
         code: lambda.DockerImageCode.fromImageAsset(rootDir, {
-          buildArgs: {
-            NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING: recaptchaKey,
-          },
           file: path.join("apps", appName, "Dockerfile"),
         }),
         environment: {
