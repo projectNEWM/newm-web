8745847473a52f42553ba0c4de96aaa2117dc57d
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-05-28 18:35:27 -0700

    removes NODE_ENV from tools and wallet deploy

--- .github/workflows/cdk/bin/deploy.ts
+++ .github/workflows/cdk/bin/deploy.ts
@@ -12,7 +12,6 @@ const appId = process.env.APPID || "APPID";
 const qualifier = process.env.QUALIFIER || "UNDEFINED";
 const recaptchaKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING || "";
 const appEnv = process.env.NEXT_PUBLIC_ENV || "";
-const nodeEnv = process.env.NODE_ENV;
 const rootDir = path.resolve(__dirname, "..", "..", "..", "..");
 
 class WebDeployStack extends cdk.Stack {
@@ -27,7 +26,6 @@ class WebDeployStack extends cdk.Stack {
           buildArgs: {
             NEXT_PUBLIC_ENV: appEnv,
             NEXT_PUBLIC_RECAPTCHA_SITE_KEY_STAGING: recaptchaKey,
-            NODE_ENV: nodeEnv,
           },
           file: path.join("apps", appName, "Dockerfile"),
         }),
