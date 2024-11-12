79a23c01c3dd00730d4c1425783af5df70791be3
escobarjonatan <escobarjonatan11@gmail.com>
2024-11-12 09:20:58 -0600

    MRKT-171: Add marketplace launch banner (#797)

--- apps/marketplace/src/app/page.tsx
+++ apps/marketplace/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { FunctionComponent } from "react";
 import { Container } from "@mui/material";
-import { ArtistSpotlight, RecentSongs } from "../components";
+import { useFlags } from "launchdarkly-react-client-sdk";
+import { ArtistSpotlight, LaunchBanner, RecentSongs } from "../components";
 
 const Home: FunctionComponent = () => {
+  const { webMarketplaceLaunchBanner } = useFlags();
   return (
     <Container sx={ { flexGrow: 1 } }>
+      { webMarketplaceLaunchBanner && <LaunchBanner /> }
       <RecentSongs />
 
       <ArtistSpotlight />
