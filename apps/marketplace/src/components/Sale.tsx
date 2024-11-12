3c5b770de44540393b4be958e7f07e750ec8061c
escobarjonatan <escobarjonatan11@gmail.com>
2024-11-12 09:17:15 -0600

    MRKT-176: Update copy for the tooltip on sale item page (#796)
    * MRKT-176: Update copy for the tooltip on sale item page
    
    * MRKT-148: Update error message copy for token purchase
    
    * MRKT-148: Remove temp open state

--- apps/marketplace/src/components/Sale.tsx
+++ apps/marketplace/src/components/Sale.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import HelpIcon from "@mui/icons-material/Help";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import currency from "currency.js";
@@ -63,7 +70,10 @@ const Sale: FunctionComponent<SaleProps> = ({
       .required("This field is required")
       .integer()
       .min(1)
-      .max(sale?.availableBundleQuantity || 0),
+      .max(
+        sale?.availableBundleQuantity || 0,
+        "Order amount cannot exceed maximum Stream Tokens available."
+      ),
   });
 
   /**
@@ -252,13 +262,31 @@ const Sale: FunctionComponent<SaleProps> = ({
                   <Stack>
                     <Box alignSelf="flex-end" p={ 0.5 }>
                       <Tooltip
-                        title={ `The number of Stream Tokens correlates directly
-                          with the percentage of Streaming royalties you
-                          can acquire and the total price of the bundle.
-                          For example 1 token is worth = 
-                          ${getPercentageOfTotalStreamTokens(1)}% of
-                          total royalties, and costs 
-                          '${formatNewmAmount(sale.costAmountNewm)}'.` }
+                        title={
+                          <Typography>
+                            The number of Stream Tokens correlates with the
+                            percentage of streaming royalties you can acquire.
+                            For example, 1 Stream Token equates to{ " " }
+                            { `${getPercentageOfTotalStreamTokens(1)}` }% of the
+                            track&apos;s total streaming royalties. For
+                            information on what constitutes a streaming royalty,
+                            please refer to the{ " " }
+                            <span style={ { color: theme.colors.music } }>
+                              <Link
+                                href={ sale.song.tokenAgreementUrl }
+                                rel="noopener noreferrer"
+                                sx={ {
+                                  width: "fit-content",
+                                } }
+                                target="_blank"
+                                variant="h4"
+                              >
+                                stream token contract
+                              </Link>
+                            </span>
+                            .
+                          </Typography>
+                        }
                       >
                         <IconButton sx={ { padding: 0 } }>
                           <HelpIcon sx={ { color: theme.colors.grey100 } } />
