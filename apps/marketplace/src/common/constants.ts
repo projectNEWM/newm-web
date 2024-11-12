f92bdac86153e06036f8da0730df325b566f2537
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2024-11-11 21:35:48 -0800

    updates marketplace footer links

--- apps/marketplace/src/common/constants.ts
+++ apps/marketplace/src/common/constants.ts
@@ -1,10 +1,11 @@
 /**
  * NEWM External Links and Support
  */
-export const NEWM_IO_URL = "https://newm.io/";
-export const NEWM_MARKETPLACE_FAQ_URL = "";
-export const NEWM_MARKETPLACE_SUPPORT_URL = "";
-export const NEWM_MARKETPLACE_TERMS_OF_SERVICE_URL = "";
+export const NEWM_IO_URL = "https://newm.io";
+export const NEWM_MARKETPLACE_FAQ_URL = "https://newm.io/faq";
+export const NEWM_MARKETPLACE_SUPPORT_URL =
+  "https://projectnewm.atlassian.net/servicedesk/customer/portal/1";
+export const NEWM_MARKETPLACE_TERMS_OF_SERVICE_URL = "https://newm.io/tos";
 export const NEWM_SUPPORT_EMAIL = "support@newm.io";
 export const NEWM_TELEGRAM_URL = "https://t.me/NEWMofficial";
 export const NEWM_X_URL = "https://twitter.com/projectNEWM";
