185a893c5f79c5bfb5cf5acc7040f5531e6f2639
dmkirshon <kirshon.david@gmail.com>
2025-02-07 15:01:45 +0100

    refactor: Simplify UploadSong for 404 component

--- apps/studio/src/pages/home/uploadSong/UploadSong.tsx
+++ apps/studio/src/pages/home/uploadSong/UploadSong.tsx
@@ -1,10 +1,11 @@
 import { Box, Container, Typography } from "@mui/material";
 import { WizardForm } from "@newm-web/elements";
 import { FormikHelpers, FormikValues } from "formik";
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useCallback, useEffect, useMemo } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { PageNotFound } from "@newm-web/components";
+import { removeTrailingSlash } from "@newm-web/utils";
 import AdvancedSongDetails from "./AdvancedSongDetails";
 import BasicSongDetails from "./BasicSongDetails";
 import ConfirmAgreement from "./ConfirmAgreement";
@@ -23,15 +24,15 @@ import {
   useUploadSongThunk,
 } from "../../../modules/song";
 
+const rootPath = "home/upload-song";
+
 export interface UploadSongFormValues extends UploadSongThunkRequest {
   agreesToCoverArtGuidelines?: boolean;
 }
 
 const UploadSong: FunctionComponent = () => {
   const navigate = useNavigate();
-  const location = useLocation();
-
-  const [isValidPath, setIsValidPath] = useState(true);
+  const currentPathLocation = useLocation();
 
   const { data: genres = [] } = useGetGenresQuery();
   const { data: roles = [] } = useGetRolesQuery();
@@ -105,97 +106,142 @@ const UploadSong: FunctionComponent = () => {
     title: "",
   };
 
-  // Navigate to advanced details if minting, otherwise upload song
-  const handleSongInfo = async (
-    values: UploadSongThunkRequest,
-    helpers: FormikHelpers<FormikValues>
-  ) => {
-    if (values.isMinting) {
+  const handleSubmit = useCallback(
+    async (
+      values: UploadSongThunkRequest,
+      helpers: FormikHelpers<FormikValues>
+    ) => {
+      await uploadSong(values);
       helpers.setSubmitting(false);
-      navigate("advanced-details");
-    } else {
-      await handleSubmit(values, helpers);
-    }
-  };
+    },
+    [uploadSong]
+  );
+
+  // Navigate to advanced details if minting, otherwise upload song
+  const handleSongInfo = useCallback(
+    async (
+      values: UploadSongThunkRequest,
+      helpers: FormikHelpers<FormikValues>
+    ) => {
+      if (values.isMinting) {
+        helpers.setSubmitting(false);
+        navigate("advanced-details");
+      } else {
+        await handleSubmit(values, helpers);
+      }
+    },
+    [navigate, handleSubmit]
+  );
 
   // Prepare Artist Agreement for confirmation page
-  const handleAdvancedDetails = async (
-    values: UploadSongThunkRequest,
-    helpers: FormikHelpers<FormikValues>
-  ) => {
-    await generateArtistAgreement({
-      artistName,
-      companyName,
-      songName: values.title,
-      stageName,
-    });
+  const handleAdvancedDetails = useCallback(
+    async (
+      values: UploadSongThunkRequest,
+      helpers: FormikHelpers<FormikValues>
+    ) => {
+      await generateArtistAgreement({
+        artistName,
+        companyName,
+        songName: values.title,
+        stageName,
+      });
 
-    helpers.setSubmitting(false);
-  };
+      helpers.setSubmitting(false);
+    },
+    [generateArtistAgreement, artistName, companyName, stageName]
+  );
 
-  const handleSubmit = async (
-    values: UploadSongThunkRequest,
-    helpers: FormikHelpers<FormikValues>
-  ) => {
-    await uploadSong(values);
-    helpers.setSubmitting(false);
-  };
+  const wizardRoutes = useMemo(
+    () => [
+      {
+        element: <BasicSongDetails />,
+        navigateOnSubmitStep: false,
+        onSubmitStep: handleSongInfo,
+        path: "",
+        progressStepTitle: "Basic details",
+        validationSchema: Yup.object().shape({
+          agreesToCoverArtGuidelines:
+            commonYupValidation.agreesToCoverArtGuidelines,
+          audio: commonYupValidation.audio,
+          coverArtUrl: commonYupValidation.coverArtUrl,
+          creditors: commonYupValidation.creditors(roles),
+          description: commonYupValidation.description,
+          genres: commonYupValidation.genres(genres),
+          moods: commonYupValidation.moods,
+          owners: commonYupValidation.owners,
+          title: commonYupValidation.title,
+        }),
+      },
+      {
+        element: <AdvancedSongDetails />,
+        onSubmitStep: handleAdvancedDetails,
+        path: "advanced-details",
+        progressStepTitle: "Advanced details",
+        validationSchema: Yup.object({
+          barcodeNumber: commonYupValidation.barcodeNumber,
+          barcodeType: commonYupValidation.barcodeType,
+          compositionCopyrightOwner: commonYupValidation.copyright,
+          compositionCopyrightYear: commonYupValidation.year,
+          ipi: commonYupValidation.ipi,
+          isrc: commonYupValidation.isrc(languageCodes),
+          iswc: commonYupValidation.iswc,
+          phonographicCopyrightOwner: commonYupValidation.copyright,
+          phonographicCopyrightYear: commonYupValidation.year,
+          publicationDate: commonYupValidation.publicationDate,
+          releaseDate: commonYupValidation.releaseDate(earliestReleaseDate),
+        }),
+      },
+      {
+        element: <ConfirmAgreement />,
+        path: "confirm",
+        progressStepTitle: "Distribute & Mint",
+        validationSchema: Yup.object().shape({
+          consentsToContract: commonYupValidation.consentsToContract,
+        }),
+      },
+    ],
+    [
+      handleSongInfo,
+      roles,
+      genres,
+      handleAdvancedDetails,
+      languageCodes,
+      earliestReleaseDate,
+    ]
+  );
+
+  /**
+   * Check if the current path is a valid path, if not, show a 404 page.
+   */
+  const currentPathName = removeTrailingSlash(currentPathLocation.pathname);
+  const validPaths = useMemo(
+    () =>
+      wizardRoutes.map((route) =>
+        removeTrailingSlash(`/${rootPath}/${route.path}`)
+      ),
+    [wizardRoutes]
+  );
+  const isValidPath = validPaths.includes(currentPathName);
 
   /**
    * Ensure user is returned to first step on refresh since form
-   * contents are not persisted. If user navigates to an invalid
-   * path, redirect to 404 page.
+   * contents are not persisted.
    *
    * TODO: remove the navigation when form values are persisted on refresh
    */
   useEffect(() => {
-    const validPaths = [
-      "/home/upload-song",
-      "/home/upload-song/advanced-details",
-      "/home/upload-song/confirm",
-    ];
-
-    // Remove trailing slashes to compare paths
-    const normalizePath = (path: string) => path.replace(/\/+$/, "");
-    const currentPath = normalizePath(location.pathname);
-
-    if (!validPaths.includes(currentPath)) {
-      setIsValidPath(false);
-    } else {
-      setIsValidPath(true);
+    if (isValidPath) {
       navigate("/home/upload-song", { replace: true });
     }
     // useNavigate doesn't return memoized function, including it
     // as dependency will run this when navigation occurs. Exclude
     // to only run on mount.
     // eslint-disable-next-line
-  }, [location.pathname]);
+  }, []);
 
   if (!isValidPath) {
     return <PageNotFound redirectUrl="/home/upload-song" />;
   }
-
-  const validations = {
-    agreesToCoverArtGuidelines: commonYupValidation.agreesToCoverArtGuidelines,
-    audio: commonYupValidation.audio,
-    barcodeNumber: commonYupValidation.barcodeNumber,
-    barcodeType: commonYupValidation.barcodeType,
-    consentsToContract: commonYupValidation.consentsToContract,
-    copyrightOwner: commonYupValidation.copyright,
-    copyrightYear: commonYupValidation.year,
-    coverArtUrl: commonYupValidation.coverArtUrl,
-    creditors: commonYupValidation.creditors(roles),
-    description: commonYupValidation.description,
-    genres: commonYupValidation.genres(genres),
-    ipi: commonYupValidation.ipi,
-    isrc: commonYupValidation.isrc(languageCodes),
-    iswc: commonYupValidation.iswc,
-    moods: commonYupValidation.moods,
-    owners: commonYupValidation.owners,
-    publicationDate: commonYupValidation.publicationDate,
-    releaseDate: commonYupValidation.releaseDate(earliestReleaseDate),
-    title: commonYupValidation.title,
-  };
 
   return (
     <Container
@@ -217,55 +263,8 @@ const UploadSong: FunctionComponent = () => {
           enableReinitialize={ true }
           initialValues={ initialValues }
           isProgressStepperVisible={ true }
-          rootPath="home/upload-song"
-          routes={ [
-            {
-              element: <BasicSongDetails />,
-              navigateOnSubmitStep: false,
-              onSubmitStep: handleSongInfo,
-              path: "",
-              progressStepTitle: "Basic details",
-              validationSchema: Yup.object().shape({
-                agreesToCoverArtGuidelines:
-                  validations.agreesToCoverArtGuidelines,
-                audio: validations.audio,
-                coverArtUrl: validations.coverArtUrl,
-                creditors: validations.creditors,
-                description: validations.description,
-                genres: validations.genres,
-                moods: validations.moods,
-                owners: validations.owners,
-                title: validations.title,
-              }),
-            },
-            {
-              element: <AdvancedSongDetails />,
-              onSubmitStep: handleAdvancedDetails,
-              path: "advanced-details",
-              progressStepTitle: "Advanced details",
-              validationSchema: Yup.object({
-                barcodeNumber: validations.barcodeNumber,
-                barcodeType: validations.barcodeType,
-                compositionCopyrightOwner: validations.copyrightOwner,
-                compositionCopyrightYear: validations.copyrightYear,
-                ipi: validations.ipi,
-                isrc: validations.isrc,
-                iswc: validations.iswc,
-                phonographicCopyrightOwner: validations.copyrightOwner,
-                phonographicCopyrightYear: validations.copyrightYear,
-                publicationDate: validations.publicationDate,
-                releaseDate: validations.releaseDate,
-              }),
-            },
-            {
-              element: <ConfirmAgreement />,
-              path: "confirm",
-              progressStepTitle: "Distribute & Mint",
-              validationSchema: Yup.object().shape({
-                consentsToContract: validations.consentsToContract,
-              }),
-            },
-          ] }
+          rootPath={ rootPath }
+          routes={ wizardRoutes }
           onSubmit={ handleSubmit }
         />
       </Box>
