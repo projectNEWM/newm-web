ed526ef54983b660f0a72538a52d5906a5b50ed9
Trevor Scandalios <5877597+scandycuz@users.noreply.github.com>
2025-04-15 20:44:39 -0700

    fixes DSP confirm plan bug

--- apps/studio/src/pages/home/uploadSong/BasicSongDetails.tsx
+++ apps/studio/src/pages/home/uploadSong/BasicSongDetails.tsx
@@ -110,9 +110,13 @@ const BasicSongDetails: FunctionComponent<BasicDonDetailsProps> = ({
 
   // DSP pricing plan mint song toggling
   const [isPricingPlansOpen, setIsPricingPlansOpen] = useState(false);
-  const handlePricingPlanClose = () => {
+  const handlePricingPlanCancel = () => {
     setIsPricingPlansOpen(false);
     setFieldValue("isMinting", false);
+  };
+  const handlePricingPlanConfirm = () => {
+    setIsPricingPlansOpen(false);
+    setFieldValue("isMinting", true);
   };
   const handlePricingPlanOpen = () => {
     setIsPricingPlansOpen(true);
@@ -196,7 +200,8 @@ const BasicSongDetails: FunctionComponent<BasicDonDetailsProps> = ({
       { !isArtistPricePlanSelected && (
         <PricingPlansDialog
           open={ isPricingPlansOpen }
-          onClose={ handlePricingPlanClose }
+          onCancel={ handlePricingPlanCancel }
+          onConfirm={ handlePricingPlanConfirm }
         />
       ) }
       <Stack direction="column" spacing={ 3 }>
