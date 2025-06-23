634870637dcbdd2bedd4d680018f825f0a244139
dmkirshon <kirshon.david@gmail.com>
2025-06-23 11:57:28 +0200

    fix: Add song editing statuses for pay tx errors
    Add additional minting statuses to allow users to edit songs
    even after payment issues, improving user experience during
    the minting process.

--- apps/studio/src/common/song.ts
+++ apps/studio/src/common/song.ts
@@ -3,6 +3,9 @@ import { MintingStatus } from "@newm-web/types";
 export const isSongEditable = (mintingStatus: MintingStatus) => {
   return (
     mintingStatus === MintingStatus.Undistributed ||
-    mintingStatus === MintingStatus.Declined
+    mintingStatus === MintingStatus.Declined ||
+    // Keeps user on edit flow even after payments fail after a tx error
+    mintingStatus === MintingStatus.MintingPaymentRequested ||
+    mintingStatus === MintingStatus.StreamTokenAgreementApproved
   );
 };
