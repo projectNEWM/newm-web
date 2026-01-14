f7b094dcafb6678e8f8b382d1f3b03a3ddc6cf57
ITurres <emanueliturres.co@gmail.com>
2026-01-14 13:43:54 -0300

    chore(packages/elements): move 'GradientDashedOutline' unit test to 'test/' dir

--- packages/elements/src/lib/test/GradientDashedOutline.test.tsx
+++ packages/elements/src/lib/test/GradientDashedOutline.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import GradientDashedOutline from "./GradientDashedOutline";
+import GradientDashedOutline from "../styled/GradientDashedOutline";
 
 const getEmotionClassName = (className: string): string | undefined =>
   className
