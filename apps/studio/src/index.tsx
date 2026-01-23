f87cddcefebc80a4ef550108ea82881e162fbb5c
ITurres <emanueliturres.co@gmail.com>
2026-01-23 14:40:59 -0300

    fix(studio): add LD loader and fallback

--- apps/studio/src/index.tsx
+++ apps/studio/src/index.tsx
@@ -4,6 +4,7 @@ import "global.css";
 import moment from "moment/min/moment-with-locales";
 import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { LAUNCHDARKLY_CLIENT_ID } from "@newm-web/env";
+import theme from "@newm-web/theme";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import initializeSentry from "./sentryConfig";
@@ -32,22 +33,51 @@ if (!isMac) {
   document.documentElement.classList.add("modify-scrollbar");
 }
 
-asyncWithLDProvider({
-  clientSideID: LAUNCHDARKLY_CLIENT_ID,
-  context: ldContext,
-  timeout: 5,
-}).then((StudioLDProvider) => {
-  const rootDomNode = document.getElementById("root") as HTMLElement;
-  if (rootDomNode) {
-    ReactDOM.createRoot(rootDomNode).render(
-      <React.StrictMode>
-        <StudioLDProvider>
+const rootDomNode = document.getElementById("root") as HTMLElement;
+
+if (rootDomNode) {
+  const root = ReactDOM.createRoot(rootDomNode);
+
+  root.render(
+    <React.StrictMode>
+      <div
+        style={ {
+          backgroundColor: theme.colors.black,
+          height: "100vh",
+          margin: 0,
+          padding: 0,
+          width: "100vw",
+        } }
+      />
+    </React.StrictMode>
+  );
+
+  asyncWithLDProvider({
+    clientSideID: LAUNCHDARKLY_CLIENT_ID,
+    context: ldContext,
+    timeout: 5,
+  })
+    .then((StudioLDProvider) => {
+      root.render(
+        <React.StrictMode>
+          <StudioLDProvider>
+            <App />
+          </StudioLDProvider>
+        </React.StrictMode>
+      );
+    })
+    .catch((error) => {
+      // * Keep this fallback: asyncWithLDProvider rarely rejects, but if it does we still render.
+      // eslint-disable-next-line no-console
+      console.error("Failed to initialize LaunchDarkly:", error);
+
+      root.render(
+        <React.StrictMode>
           <App />
-        </StudioLDProvider>
-      </React.StrictMode>
-    );
-  }
-});
+        </React.StrictMode>
+      );
+    });
+}
 
 // * If you want to start measuring performance in your app, pass a function
 // * to log results (for example: reportWebVitals(console.log))
