73aa4c34d8a136ab36ebef957c84fc8de99c309d
ITurres <emanueliturres.co@gmail.com>
2026-01-28 15:03:23 -0300

    feat(studio/releases): add new track create flow
    - add 'release/tracks/NewTrack' with combined basic/advanced sections
    - add basic/advanced track detail components and form types
    - remove legacy 'TrackDetails' page

---
+++ apps/studio/src/pages/home/releases/release/tracks/BasicTrackDetails.tsx
@@ -0,0 +1,376 @@
+import { FunctionComponent, useEffect, useRef, useState } from "react";
+
+import { useFormikContext } from "formik";
+
+import { useFlags } from "launchdarkly-react-client-sdk";
+
+import { Box, Link, Stack, Typography, useTheme } from "@mui/material";
+
+import {
+  CheckboxField,
+  DropdownMultiSelectField,
+  DropdownSelectField,
+  ErrorMessage,
+  SolidOutline,
+  TextAreaField,
+  TextInputField,
+  UploadImageField,
+  UploadSongField,
+} from "@newm-web/elements";
+import {
+  LocalStorage,
+  scrollToError,
+  useExtractProperty,
+} from "@newm-web/utils";
+import { LocalStorageKey } from "@newm-web/types";
+
+import { TrackFormValues } from "./trackFormTypes";
+import {
+  NEWM_STUDIO_FAQ_URL,
+  SONG_DESCRIPTION_MAX_CHARACTER_COUNT,
+} from "../../../../../common";
+import {
+  DistributionPricingDialog,
+  PlaySong,
+  PricingPlansDialog,
+} from "../../../../../components";
+import SelectCoCreators from "../../../../../components/minting/SelectCoCreators";
+import {
+  useGetGenresQuery,
+  useGetLanguagesQuery,
+  useGetMoodsQuery,
+} from "../../../../../modules/content";
+import {
+  emptyProfile,
+  useGetProfileQuery,
+} from "../../../../../modules/session";
+import { Creditor, Featured, Owner } from "../../../../../modules/song";
+
+interface BasicTrackDetailsProps {
+  readonly isDeclined?: boolean;
+  readonly isInEditMode?: boolean;
+  readonly trackId?: string;
+}
+
+const BasicTrackDetails: FunctionComponent<BasicTrackDetailsProps> = ({
+  isDeclined = false,
+  isInEditMode = false,
+  trackId = "",
+}) => {
+  const { webStudioReleaseDistributionPaymentEnhancements } = useFlags();
+
+  const theme = useTheme();
+  const {
+    data: {
+      dspPlanSubscribed: isArtistPricePlanSelected = false,
+    } = emptyProfile,
+  } = useGetProfileQuery();
+  const { webStudioDisableTrackDistributionAndMinting } = useFlags();
+  const { data: genres = [] } = useGetGenresQuery();
+  const { data: moodOptions = [] } = useGetMoodsQuery();
+  const { data: languages = [] } = useGetLanguagesQuery();
+  const languageOptions = useExtractProperty(languages, "language_name");
+
+  const audioRef = useRef<HTMLDivElement>(null);
+  const coCreatorsRef = useRef<HTMLDivElement>(null);
+  const coverArtUrlRef = useRef<HTMLDivElement>(null);
+  const agreesToCoverArtGuidelinesRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const songDetailsRef = useRef<HTMLDivElement>(null);
+
+  const {
+    values,
+    errors,
+    touched,
+    setFieldValue,
+    isSubmitting,
+    initialValues,
+  } = useFormikContext<TrackFormValues>();
+
+  // DSP pricing plan mint song toggling
+  const [isPricingPlansOpen, setIsPricingPlansOpen] = useState(false);
+  const handlePricingPlanCancel = () => {
+    setIsPricingPlansOpen(false);
+    setFieldValue("isMinting", false);
+  };
+  const handlePricingPlanConfirm = () => {
+    setIsPricingPlansOpen(false);
+    setFieldValue("isMinting", true);
+  };
+
+  // Handle the one-time pricing plan acceptance
+  useEffect(() => {
+    const hasAcceptedPricingPlan =
+      LocalStorage.getItem(LocalStorageKey.isStudioPricingPlanAccepted) ===
+      "true";
+
+    if (hasAcceptedPricingPlan) {
+      if (!webStudioDisableTrackDistributionAndMinting) {
+        setFieldValue("isMinting", true);
+      }
+      LocalStorage.removeItem(LocalStorageKey.isStudioPricingPlanAccepted);
+    }
+  }, [setFieldValue, webStudioDisableTrackDistributionAndMinting]);
+
+  // Monitor feature flag changes, particularly seen for pricing plan acceptance
+  useEffect(() => {
+    if (webStudioDisableTrackDistributionAndMinting) {
+      setFieldValue("isMinting", false);
+    }
+  }, [webStudioDisableTrackDistributionAndMinting, setFieldValue]);
+
+  const hasCoverArtChanged = values.coverArtUrl !== initialValues.coverArtUrl;
+
+  const handleChangeOwners = (owners: ReadonlyArray<Owner>) => {
+    setFieldValue("owners", owners);
+  };
+
+  const handleChangeCreditors = (creditors: ReadonlyArray<Creditor>) => {
+    setFieldValue("creditors", creditors);
+  };
+
+  const handleChangeFeatured = (featured: ReadonlyArray<Featured>) => {
+    setFieldValue("featured", featured);
+  };
+
+  useEffect(() => {
+    scrollToError(errors, isSubmitting, [
+      { element: audioRef.current, error: errors.audio },
+      { element: coverArtUrlRef.current, error: errors.coverArtUrl },
+      {
+        element: agreesToCoverArtGuidelinesRef.current,
+        error: errors.agreesToCoverArtGuidelines,
+      },
+      {
+        element: songDetailsRef.current,
+        error: errors.title || errors.genres || errors.moods,
+      },
+      {
+        element: descriptionRef.current,
+        error: errors.description,
+      },
+      { element: coCreatorsRef.current, error: errors.creditors },
+      { element: coCreatorsRef.current, error: errors.owners },
+    ]);
+  }, [errors, isSubmitting]);
+
+  useEffect(() => {
+    if (hasCoverArtChanged) {
+      setFieldValue("agreesToCoverArtGuidelines", false);
+    }
+  }, [setFieldValue, hasCoverArtChanged]);
+
+  return (
+    <Stack>
+      { !isArtistPricePlanSelected &&
+        (webStudioReleaseDistributionPaymentEnhancements ? (
+          <DistributionPricingDialog
+            open={ isPricingPlansOpen }
+            onCancel={ handlePricingPlanCancel }
+            onConfirm={ handlePricingPlanConfirm }
+          />
+        ) : (
+          <PricingPlansDialog
+            open={ isPricingPlansOpen }
+            onCancel={ handlePricingPlanCancel }
+            onConfirm={ handlePricingPlanConfirm }
+          />
+        )) }
+      <Stack direction="column" spacing={ 3 }>
+        <Stack
+          sx={ {
+            alignItems: ["center", "center", "unset"],
+            columnGap: [undefined, undefined, 1.5],
+            display: "flex",
+            flexDirection: ["column", "column", "row"],
+            marginBottom: 3,
+            maxWidth: [undefined, undefined, "700px"],
+            rowGap: [2, null, 3],
+          } }
+        >
+          <Stack
+            maxWidth={ theme.inputField.maxWidth }
+            ref={ audioRef }
+            spacing={ 0.5 }
+            width="100%"
+          >
+            { isInEditMode ? (
+              <>
+                <Typography color={ theme.colors.grey100 } fontWeight={ 700 }>
+                  SONG
+                </Typography>
+
+                <SolidOutline
+                  sx={ {
+                    alignItems: "center",
+                    display: "flex",
+                    flexGrow: 1,
+                    height: "100px",
+                    justifyContent: "center",
+                  } }
+                >
+                  <PlaySong id={ trackId } />
+                </SolidOutline>
+              </>
+            ) : (
+              <>
+                <Typography color={ theme.colors.grey100 } fontWeight={ 700 }>
+                  SONG FILE
+                </Typography>
+
+                <UploadSongField name="audio" />
+              </>
+            ) }
+          </Stack>
+
+          <Stack
+            maxWidth={ theme.inputField.maxWidth }
+            ref={ coverArtUrlRef }
+            spacing={ 0.5 }
+            width="100%"
+          >
+            <Typography color={ theme.colors.grey100 } fontWeight={ 700 }>
+              SONG COVER ART
+            </Typography>
+
+            <UploadImageField
+              changeImageButtonText="Change cover"
+              emptyMessage="Drag and drop or browse your image"
+              maxFileSizeMB={ 10 }
+              minDimensions={ { height: 1400, width: 1400 } }
+              minFileSizeMB={ 0.1 }
+              name="coverArtUrl"
+              rootSx={ { alignSelf: "center", width: "100%" } }
+              hasPreviewOption
+              isAspectRatioOneToOne
+            />
+          </Stack>
+        </Stack>
+
+        <Stack
+          spacing={ 3 }
+          sx={ {
+            alignSelf: ["center", "center", "unset"],
+            marginX: ["auto", "auto", "unset"],
+            maxWidth: [
+              theme.inputField.maxWidth,
+              theme.inputField.maxWidth,
+              "700px",
+            ],
+          } }
+        >
+          { hasCoverArtChanged && (
+            <CheckboxField
+              checked={ values.agreesToCoverArtGuidelines }
+              label={
+                <Typography
+                  sx={ {
+                    color: "white",
+                    fontSize: 12,
+                  } }
+                  variant="subtitle1"
+                >
+                  I confirm that the cover art meets the specified guidelines,
+                  and submitting cover art that does not comply may result in a
+                  declined track distribution. For a full list of these
+                  guidelines, please see our{ " " }
+                  <Link
+                    href={ NEWM_STUDIO_FAQ_URL }
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    FAQ
+                  </Link>
+                  .
+                </Typography>
+              }
+              name="agreesToCoverArtGuidelines"
+              ref={ agreesToCoverArtGuidelinesRef }
+            />
+          ) }
+          <Stack
+            ref={ songDetailsRef }
+            sx={ {
+              columnGap: [undefined, undefined, 1.5],
+              display: "grid",
+              gridTemplateColumns: ["repeat(1, 1fr)", null, "repeat(2, 1fr)"],
+              rowGap: [2, null, 3],
+            } }
+          >
+            <TextInputField
+              isOptional={ false }
+              label="SONG TITLE"
+              name="title"
+              placeholder="Give your track a name..."
+            />
+
+            <DropdownMultiSelectField
+              isOptional={ false }
+              label="GENRE"
+              name="genres"
+              options={ genres }
+              placeholder="Select all that apply"
+            />
+
+            <DropdownSelectField
+              label="LANGUAGE"
+              name="language"
+              options={ languageOptions }
+              placeholder="Select a language"
+            />
+
+            <DropdownMultiSelectField
+              label="MOOD"
+              name="moods"
+              options={ moodOptions }
+              placeholder="Select all that apply"
+            />
+          </Stack>
+
+          <TextAreaField
+            characterCountLimit={ SONG_DESCRIPTION_MAX_CHARACTER_COUNT }
+            currentCharacterCount={ values.description?.length }
+            label="DESCRIPTION"
+            name="description"
+            placeholder="Tell us about your song"
+            ref={ descriptionRef }
+          />
+
+          <Stack spacing={ 3 }>
+            <Box
+              sx={ {
+                backgroundColor: theme.colors.grey600,
+                border: `2px solid ${theme.colors.grey400}`,
+                borderRadius: "4px",
+              } }
+            >
+              <SelectCoCreators
+                creditors={ values.creditors }
+                featured={ values.featured }
+                isAddDeleteDisabled={ isDeclined }
+                owners={ values.owners }
+                onChangeCreditors={ handleChangeCreditors }
+                onChangeFeatured={ handleChangeFeatured }
+                onChangeOwners={ handleChangeOwners }
+              />
+            </Box>
+
+            { !!touched.owners && !!errors.owners && (
+              <Box mt={ 0.5 }>
+                <ErrorMessage>{ errors.owners as string }</ErrorMessage>
+              </Box>
+            ) }
+
+            { !!touched.creditors && !!errors.creditors && (
+              <Box mt={ 0.5 }>
+                <ErrorMessage>{ errors.creditors as string }</ErrorMessage>
+              </Box>
+            ) }
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default BasicTrackDetails;
---
+++ apps/studio/src/pages/home/releases/release/tracks/EditTrack.tsx
@@ -0,0 +1,424 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { useDispatch } from "react-redux";
+
+import { useFlags } from "launchdarkly-react-client-sdk";
+
+import { Form, Formik, FormikHelpers } from "formik";
+
+import * as Yup from "yup";
+
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Stack, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+import { Button, HorizontalLine, Tooltip } from "@newm-web/elements";
+import { useWindowDimensions } from "@newm-web/utils";
+import { MintingStatus, PaymentType } from "@newm-web/types";
+
+import AdvancedTrackDetails from "./AdvancedTrackDetails";
+import BasicTrackDetails from "./BasicTrackDetails";
+import DeleteSongModal from "../../DeleteSongModal";
+import ReleaseDeletionHelp from "../../ReleaseDeletionHelp";
+import { commonYupValidation, isSongEditable } from "../../../../../common";
+import {
+  useGetGenresQuery,
+  useGetISRCCountryCodesQuery,
+  useGetRolesQuery,
+} from "../../../../../modules/content";
+import {
+  emptyProfile,
+  useGetProfileQuery,
+} from "../../../../../modules/session";
+import {
+  CollaborationStatus,
+  PatchSongThunkRequest,
+  emptySong,
+  getIsSongDeletable,
+  useDeleteSongThunk,
+  useGetCollaborationsQuery,
+  useGetEarliestReleaseDateQuery,
+  useGetSongQuery,
+  useHasSongAccess,
+  usePatchSongThunk,
+} from "../../../../../modules/song";
+import { setToastMessage } from "../../../../../modules/ui";
+
+interface EditTrackFormValues extends PatchSongThunkRequest {
+  agreesToCoverArtGuidelines?: boolean;
+}
+
+const EditTrack: FunctionComponent = () => {
+  const { webStudioDisableTrackDistributionAndMinting } = useFlags();
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  const { releaseId, trackId } = useParams<"releaseId" | "trackId">() as {
+    releaseId?: string;
+    trackId?: string;
+  };
+  const windowWidth = useWindowDimensions()?.width;
+
+  const { data: genres = [] } = useGetGenresQuery();
+  const { data: roles = [] } = useGetRolesQuery();
+  const {
+    data: {
+      companyName = "",
+      dspPlanSubscribed: isArtistPricePlanSelected = false,
+      email,
+      firstName = "",
+      lastName = "",
+      nickname: stageName = "",
+      role,
+    } = emptyProfile,
+  } = useGetProfileQuery();
+  const { data: languageCodes = [] } = useGetISRCCountryCodesQuery();
+
+  const { data: collaborations = [] } = useGetCollaborationsQuery({
+    songIds: trackId ?? "",
+  });
+  const { data: { date: earliestReleaseDate } = {} } =
+    useGetEarliestReleaseDateQuery();
+
+  const [patchSong] = usePatchSongThunk();
+
+  const hasAccess = useHasSongAccess(trackId ?? "");
+  const artistName = `${firstName} ${lastName}`;
+
+  const [deleteSong] = useDeleteSongThunk();
+
+  const [isDeleteModalActive, setIsDeleteModalActive] = useState(false);
+  const {
+    data: {
+      barcodeNumber,
+      barcodeType,
+      compositionCopyrightOwner,
+      compositionCopyrightYear,
+      coverArtUrl,
+      description,
+      genres: songGenres,
+      instrumental,
+      ipis,
+      isrc,
+      iswc,
+      language,
+      mintingStatus,
+      moods,
+      parentalAdvisory,
+      phonographicCopyrightOwner,
+      phonographicCopyrightYear,
+      publicationDate,
+      releaseDate,
+      title,
+    } = emptySong,
+    error,
+  } = useGetSongQuery(trackId ?? "");
+
+  const isSongDeletable = getIsSongDeletable(mintingStatus);
+
+  const isDeclined = mintingStatus === MintingStatus.Declined;
+
+  const owners = collaborations
+    .filter(
+      (collaboration) =>
+        collaboration.royaltyRate && collaboration.royaltyRate > 0
+    )
+    .map((collaboration) => ({
+      email: collaboration.email,
+      isCreator: "isCreator" in collaboration && !!collaboration.isCreator,
+      isRightsOwner: collaboration.royaltyRate
+        ? collaboration.royaltyRate > 0
+        : false,
+      percentage:
+        collaboration.royaltyRate !== undefined
+          ? +collaboration.royaltyRate
+          : 0,
+
+      role: collaboration.role,
+      status: collaboration.status,
+    }));
+
+  const creditors = collaborations
+    .filter((collaboration) => collaboration.credited)
+    .map((collaboration) => ({
+      email: collaboration.email,
+      isCredited: collaboration.credited,
+      role: collaboration.role,
+      status: collaboration.status,
+    }));
+
+  const featured = collaborations
+    .filter((collaboration) => collaboration.featured)
+    .map((collaboration) => ({
+      email: collaboration.email,
+      isFeatured: collaboration.featured,
+      role: collaboration.role,
+      status: collaboration.status,
+    }));
+
+  const initialValues: EditTrackFormValues = {
+    agreesToCoverArtGuidelines: true,
+    artistName,
+    barcodeNumber,
+    barcodeType,
+    companyName,
+    compositionCopyrightOwner,
+    compositionCopyrightYear,
+    consentsToContract: false,
+    coverArtUrl,
+    creditors:
+      creditors.length > 0
+        ? creditors
+        : [
+            {
+              email,
+              isCredited: true,
+              role,
+              status: CollaborationStatus.Editing,
+            },
+          ],
+    description,
+    featured,
+    genres: songGenres,
+    id: trackId ?? "",
+    ipi: ipis?.join(", "),
+    isExplicit: parentalAdvisory === "Explicit",
+    isInstrumental: instrumental,
+    isMinting:
+      isArtistPricePlanSelected &&
+      isSongEditable(mintingStatus) &&
+      !webStudioDisableTrackDistributionAndMinting,
+    isrc,
+    iswc,
+    language,
+    moods,
+    owners:
+      owners.length > 0
+        ? owners
+        : [
+            {
+              email,
+              isCreator: true,
+              isRightsOwner: true,
+              percentage: 100,
+              role,
+              status: CollaborationStatus.Editing,
+            },
+          ],
+    paymentType: PaymentType.ADA,
+    phonographicCopyrightOwner,
+    phonographicCopyrightYear,
+    publicationDate,
+    releaseDate,
+    stageName,
+    title,
+  };
+
+  // TODO: show "Not found" content if not available for user
+  if (error || !hasAccess) {
+    navigate("/home/releases");
+
+    dispatch(
+      setToastMessage({
+        message: "Error fetching song data",
+        severity: "error",
+      })
+    );
+  }
+
+  const handleSubmit = async (
+    values: EditTrackFormValues,
+    helpers: FormikHelpers<EditTrackFormValues>
+  ) => {
+    await patchSong({
+      ...values,
+      mintingStatus,
+      shouldRedirect: values.isMinting,
+    });
+
+    helpers.setSubmitting(false);
+
+    if (releaseId) {
+      navigate(`/home/releases/${releaseId}`);
+    } else {
+      navigate("/home/releases");
+    }
+  };
+
+  /**
+   * Ensure user is returned to first step on refresh since form
+   * contents are not persisted.
+   *
+   * TODO: remove this when form values are persisted on refresh
+   */
+  useEffect(() => {
+    if (releaseId && trackId) {
+      navigate(`/home/releases/${releaseId}/track/${trackId}`, {
+        replace: true,
+      });
+    }
+    // useNavigate doesn't return memoized function, including it
+    // as dependency will run this when navigation occurs. Exclude
+    // to only run on mount.
+    // eslint-disable-next-line
+  }, []);
+
+  const validations = {
+    agreesToCoverArtGuidelines: commonYupValidation.agreesToCoverArtGuidelines,
+    barcodeNumber: commonYupValidation.barcodeNumber,
+    barcodeType: commonYupValidation.barcodeType,
+    copyrightOwner: commonYupValidation.copyright,
+    copyrightYear: commonYupValidation.year,
+    coverArtUrl: commonYupValidation.coverArtUrl,
+    creditors: commonYupValidation.creditors(roles),
+    description: commonYupValidation.description,
+    genres: commonYupValidation.genres(genres),
+    ipi: commonYupValidation.ipi,
+    isrc: commonYupValidation.isrc(languageCodes),
+    iswc: commonYupValidation.iswc,
+    moods: commonYupValidation.moods,
+    owners: commonYupValidation.owners,
+    publicationDate: commonYupValidation.publicationDate,
+    releaseDate: commonYupValidation.releaseDate(
+      earliestReleaseDate,
+      isDeclined
+    ),
+    title: commonYupValidation.title,
+  };
+
+  return (
+    <>
+      <Stack alignItems="center" direction="row" gap={ 2.5 }>
+        <Button
+          color="white"
+          variant="outlined"
+          width="icon"
+          onClick={ () => navigate(-1) }
+        >
+          <ArrowBackIcon sx={ { color: "white" } } />
+        </Button>
+
+        { title && <Typography variant="h3">{ title.toUpperCase() }</Typography> }
+
+        <>
+          <Tooltip
+            disableFocusListener={ isSongDeletable }
+            disableHoverListener={ isSongDeletable }
+            disableTouchListener={ isSongDeletable }
+            title={ isSongDeletable ? "" : <ReleaseDeletionHelp /> }
+          >
+            <Stack ml="auto">
+              <Button
+                color="white"
+                // render other status for
+                disabled={ !isSongDeletable }
+                sx={ { marginLeft: "auto" } }
+                variant="outlined"
+                width="icon"
+                onClick={ () => {
+                  setIsDeleteModalActive(true);
+                } }
+              >
+                <DeleteIcon fontSize="small" sx={ { color: "white" } } />
+              </Button>
+            </Stack>
+          </Tooltip>
+
+          { isDeleteModalActive && (
+            <DeleteSongModal
+              primaryAction={ () => {
+                deleteSong({
+                  archived: true,
+                  songId: trackId ?? "",
+                });
+              } }
+              secondaryAction={ () => {
+                setIsDeleteModalActive(false);
+              } }
+            />
+          ) }
+        </>
+      </Stack>
+      <Box pb={ 7 } pt={ 5 }>
+        <Formik
+          enableReinitialize={ true }
+          initialValues={ initialValues }
+          validationSchema={ Yup.object().shape({
+            agreesToCoverArtGuidelines: validations.agreesToCoverArtGuidelines,
+            barcodeNumber: validations.barcodeNumber,
+            barcodeType: validations.barcodeType,
+            compositionCopyrightOwner: validations.copyrightOwner,
+            compositionCopyrightYear: validations.copyrightYear,
+            coverArtUrl: validations.coverArtUrl,
+            creditors: validations.creditors,
+            description: validations.description,
+            genres: validations.genres,
+            ipi: validations.ipi,
+            isrc: validations.isrc,
+            iswc: validations.iswc,
+            moods: validations.moods,
+            owners: validations.owners,
+            publicationDate: validations.publicationDate,
+            releaseDate: validations.releaseDate,
+            title: validations.title,
+          }) }
+          onSubmit={ handleSubmit }
+        >
+          { ({ isSubmitting }) => (
+            <Form noValidate>
+              <Stack
+                spacing={ 6 }
+                sx={ {
+                  alignItems: ["center", "center", "unset"],
+                } }
+              >
+                <Stack spacing={ 2 }>
+                  <Typography variant="h4">BASIC DETAILS</Typography>
+                  <BasicTrackDetails
+                    isDeclined={ isDeclined }
+                    isInEditMode={ true }
+                    trackId={ trackId }
+                  />
+                </Stack>
+
+                <Stack spacing={ 2 }>
+                  <Typography variant="h4">ADVANCED DETAILS</Typography>
+                  <AdvancedTrackDetails />
+                </Stack>
+
+                <Box>
+                  <HorizontalLine mb={ 5 } />
+                  <Stack direction="row" gap={ 2 }>
+                    <Button
+                      isLoading={ isSubmitting }
+                      type="submit"
+                      width={
+                        windowWidth && windowWidth > theme.breakpoints.values.md
+                          ? "compact"
+                          : "default"
+                      }
+                    >
+                      Save
+                    </Button>
+
+                    <Button
+                      variant="outlined"
+                      width="compact"
+                      onClick={ () => navigate(-1) }
+                    >
+                      Cancel
+                    </Button>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Form>
+          ) }
+        </Formik>
+      </Box>
+    </>
+  );
+};
+
+export default EditTrack;
