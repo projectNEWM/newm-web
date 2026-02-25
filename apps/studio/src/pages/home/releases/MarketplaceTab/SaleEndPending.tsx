ae1b9340da900422069fd4b9eaddafd609aa3b43
Arthur ITurres <arthuriturres.co@gmail.com>
2026-02-25 12:09:16 -0300

    [STUD-581][Enhancement]: Implement final Marketplace Tab 'End Sale' copy edits (#979)
    * feat(studio/releases): Adjust end sale copy under 'webStudioDisableDistributionAndSales' flag
    
    Unify End Sale dialog messaging when sales are disabled.
    Keep sold-out vs active copy for default behavior.
    
    * fix(studio/releases): address small typo in copy
    
    * fix(studio/releases): address alert title style alignment at 'SaleEndPending'

--- apps/studio/src/pages/home/releases/MarketplaceTab/SaleEndPending.tsx
+++ apps/studio/src/pages/home/releases/MarketplaceTab/SaleEndPending.tsx
@@ -1,30 +1,63 @@
+import { useFlags } from "launchdarkly-react-client-sdk";
+
 import { AlertTitle, Stack, Typography } from "@mui/material";
+
 import { Alert, HorizontalLine } from "@newm-web/elements";
 import theme from "@newm-web/theme";
 
-const SaleEndPending = () => (
-  <Stack sx={ { gap: 2.5, mt: 1 } }>
-    <Alert>
-      <AlertTitle color={ theme.colors.blue } sx={ { fontWeight: 600 } }>
-        You have a stream token sale for this track that is pending
-        cancellation!
-      </AlertTitle>
-      <Typography
-        color={ theme.colors.blue }
-        fontWeight={ 500 }
-        variant="subtitle1"
-      >
-        Your sale is now being removed from the Marketplace.
-      </Typography>
-    </Alert>
-    <Typography mt={ 2.5 } variant="subtitle2">
-      Stream token sales may take several minutes to be removed from the
-      Marketplace. You will receive an email notification once the sale has been
-      successfully canceled. Once the cancellation is finalized, you can create
-      a new stream token sale for this track.
+const SunDownAlertTitle = () => (
+  <AlertTitle
+    color={ theme.colors.blue }
+    sx={ { fontWeight: 600, marginBottom: 0 } }
+  >
+    <Typography variant="subtitle1">
+      Your stream token sale is being removed from the Marketplace!
     </Typography>
-    <HorizontalLine />
-  </Stack>
+  </AlertTitle>
 );
+
+const SaleEndPending = () => {
+  const { webStudioDisableDistributionAndSales } = useFlags();
+
+  return (
+    <Stack sx={ { gap: 2.5, mt: 1 } }>
+      <Alert>
+        { webStudioDisableDistributionAndSales ? (
+          <SunDownAlertTitle />
+        ) : (
+          <>
+            <AlertTitle color={ theme.colors.blue } sx={ { fontWeight: 600 } }>
+              You have a stream token sale for this track that is pending
+              cancellation!
+            </AlertTitle>
+            <Typography
+              color={ theme.colors.blue }
+              fontWeight={ 500 }
+              variant="subtitle1"
+            >
+              Your sale is now being removed from the Marketplace.
+            </Typography>
+          </>
+        ) }
+      </Alert>
+      <Typography mt={ 2.5 } variant="subtitle2">
+        { webStudioDisableDistributionAndSales ? (
+          <>
+            Stream token sales may take several minutes to be removed. You will
+            receive an email notification once the sale has successfully ended.
+          </>
+        ) : (
+          <>
+            Stream token sales may take several minutes to be removed from the
+            Marketplace. You will receive an email notification once the sale
+            has been successfully canceled. Once the cancellation is finalized,
+            you can create a new stream token sale for this track.
+          </>
+        ) }
+      </Typography>
+      <HorizontalLine />
+    </Stack>
+  );
+};
 
 export default SaleEndPending;
