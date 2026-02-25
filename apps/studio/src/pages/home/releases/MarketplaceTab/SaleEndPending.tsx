53c5fe23e2374465d8396f6b2f9cfedfc4da2a2e
ITurres <emanueliturres.co@gmail.com>
2026-02-25 11:52:54 -0300

    fix(studio/releases): address alert title style alignment at 'SaleEndPending'

--- apps/studio/src/pages/home/releases/MarketplaceTab/SaleEndPending.tsx
+++ apps/studio/src/pages/home/releases/MarketplaceTab/SaleEndPending.tsx
@@ -5,33 +5,39 @@ import { AlertTitle, Stack, Typography } from "@mui/material";
 import { Alert, HorizontalLine } from "@newm-web/elements";
 import theme from "@newm-web/theme";
 
+const SunDownAlertTitle = () => (
+  <AlertTitle
+    color={ theme.colors.blue }
+    sx={ { fontWeight: 600, marginBottom: 0 } }
+  >
+    <Typography variant="subtitle1">
+      Your stream token sale is being removed from the Marketplace!
+    </Typography>
+  </AlertTitle>
+);
+
 const SaleEndPending = () => {
   const { webStudioDisableDistributionAndSales } = useFlags();
 
   return (
     <Stack sx={ { gap: 2.5, mt: 1 } }>
       <Alert>
-        <AlertTitle color={ theme.colors.blue } sx={ { fontWeight: 600 } }>
-          { webStudioDisableDistributionAndSales ? (
-            <Typography variant="subtitle1">
-              Your stream token sale is being removed from the Marketplace!
-            </Typography>
-          ) : (
-            <>
+        { webStudioDisableDistributionAndSales ? (
+          <SunDownAlertTitle />
+        ) : (
+          <>
+            <AlertTitle color={ theme.colors.blue } sx={ { fontWeight: 600 } }>
               You have a stream token sale for this track that is pending
               cancellation!
-            </>
-          ) }
-        </AlertTitle>
-
-        { !webStudioDisableDistributionAndSales && (
-          <Typography
-            color={ theme.colors.blue }
-            fontWeight={ 500 }
-            variant="subtitle1"
-          >
-            Your sale is now being removed from the Marketplace.
-          </Typography>
+            </AlertTitle>
+            <Typography
+              color={ theme.colors.blue }
+              fontWeight={ 500 }
+              variant="subtitle1"
+            >
+              Your sale is now being removed from the Marketplace.
+            </Typography>
+          </>
         ) }
       </Alert>
       <Typography mt={ 2.5 } variant="subtitle2">
